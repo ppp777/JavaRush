45fde6f503fa0cb6a49bf367c1bdd572cb4094ef
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-06 20:00:22 +0300

    com.javarush.test.level08.lesson06.task05 changed.

--- com/javarush/test/level08/lesson06/task05/Solution.java
+++ com/javarush/test/level08/lesson06/task05/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.test.level08.lesson06.task05;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,145 +16,22 @@ public class Solution
 {
     public static List getListForGet()
     {
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-        LinkedList<Object> linkedList = new LinkedList<Object>();
-
-        insert10000( arrayList );
-        insert10000( linkedList );
-
-        long arrayListGetTime = getTimeMsOfGet( arrayList );
-        long linkedListGetTime = getTimeMsOfGet( linkedList );
-
-        System.out.println(arrayListGetTime);     /////////////////////////////
-        System.out.println(linkedListGetTime);    /////////////////////////////
-
-        return ( arrayListGetTime < linkedListGetTime ) ? arrayList : linkedList;
+        return new ArrayList<Object>();
     }
 
     public static List getListForSet()
     {
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-        LinkedList<Object> linkedList = new LinkedList<Object>();
-
-        insert10000( arrayList );
-        insert10000( linkedList );
-
-        long arrayListSetTime = getTimeMsOfSet( arrayList );
-        long linkedListSetTime = getTimeMsOfSet( linkedList );
-
-        System.out.println(arrayListSetTime);     /////////////////////////////
-        System.out.println(linkedListSetTime);    /////////////////////////////
-
-        return ( arrayListSetTime < linkedListSetTime ) ? arrayList : linkedList;
+        return new ArrayList<Object>();
     }
 
     public static List getListForAddOrInsert()
     {
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-        LinkedList<Object> linkedList = new LinkedList<Object>();
-
-        long arrayListAddTime = getTimeMsOfInsert( arrayList );
-        long linkedListAddTime = getTimeMsOfInsert( linkedList );
-
-        System.out.println(arrayListAddTime);     /////////////////////////////
-        System.out.println(linkedListAddTime);    /////////////////////////////
-        return ( linkedListAddTime < arrayListAddTime ) ? linkedList : arrayList;
-
+        return new LinkedList<Object>();
     }
 
     public static List getListForRemove()
     {
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-        LinkedList<Object> linkedList = new LinkedList<Object>();
-
-        insert10000( arrayList );
-        insert10000( linkedList );
-
-        long arrayListRemoveTime = getTimeMsOfRemove( arrayList );
-        long linkedListRemoveTime = getTimeMsOfRemove( linkedList );
-
-        System.out.println(arrayListRemoveTime);     /////////////////////////////
-        System.out.println(linkedListRemoveTime);    /////////////////////////////
-
-        return ( arrayListRemoveTime < linkedListRemoveTime ) ? arrayList : linkedList;
-    }
-
-    private static long getTimeMsOfGet( List list )
-    {
-        long currentTime = new Date().getTime();
-
-        get10000( list );
-        return new Date().getTime() - currentTime;
-    }
-
-    private static long getTimeMsOfSet( List list )
-    {
-        long currentTime = new Date().getTime();
-
-        set10000( list );
-        return new Date().getTime() - currentTime;
-    }
-
-    private static long getTimeMsOfRemove( List list )
-    {
-        long currentTime = new Date().getTime();
-
-        remove10000( list );
-        return new Date().getTime() - currentTime;
-    }
-
-    private static long getTimeMsOfInsert( List list )
-    {
-        long currentTime = new Date().getTime();
-
-        insert10000( list );
-        return new Date().getTime() - currentTime;
-    }
-
-    private static void get10000( List list )
-    {
-        if ( list.isEmpty() )
-        {
-            return;
-        }
-        int x = list.size() / 2;
-
-        for ( int i = 0; i < 10000; i++ )
-        {
-            list.get( x );
-        }
-    }
-
-    private static void insert10000( List list )
-    {
-        for ( int i = 0; i < 10000; i++ )
-        {
-            list.add( 0, new Object() );
-        }
-    }
-
-    private static void set10000( List list )
-    {
-        for ( int i = 0; i < 10000; i++ )
-        {
-            list.set( i, new Object() );
-        }
-    }
-
-    public static void remove10000( List list )
-    {
-        for ( int i = 0; i < 10000; i++ )
-        {
-            list.remove( 0 );
-        }
-    }
-
-    public static void main( String[] args )
-    {
-        getListForGet();
-        getListForSet();
-        getListForAddOrInsert();
-        getListForRemove();
+        return new LinkedList<Object>();
     }
 }
 
