1a61ae4ff95ae279f7ebbddfca4fb2d3a3f50278
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-07 22:33:06 +0300

    com.javarush.test.level08.lesson11.home01 changed.

---
+++ com/javarush/test/level09/lesson02/task01/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.test.level09.lesson02.task01;
+
+/**
+ * Date: 07.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        method1();
+    }
+
+    public static StackTraceElement[] method1()
+    {
+        method2();
+        //Напишите тут ваш код
+
+    }
+
+    public static StackTraceElement[] method2()
+    {
+        method3();
+        //Напишите тут ваш код
+
+    }
+
+    public static StackTraceElement[] method3()
+    {
+        method4();
+        //Напишите тут ваш код
+
+    }
+
+    public static StackTraceElement[] method4()
+    {
+        method5();
+        //Напишите тут ваш код
+
+    }
+
+    public static StackTraceElement[] method5()
+    {
+        //Напишите тут ваш код
+
+    }
+}
+
+
