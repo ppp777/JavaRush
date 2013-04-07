05b306d6bfa4e64de0532bc4bbc0e68221610fdd
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-07 21:59:47 +0300

    com.javarush.test.level08.lesson11.home01 added.

---
+++ com/javarush/test/level08/lesson11/home01/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level08.lesson11.home01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * JavaRush.ru
+ * Level 08, Lesson 11, Home 01
+ * <p/>
+ * Написать класс кот - Cat. Создать множество (Set) котов. Создать три кота и добавить их в это множество. Удалить
+ * одного кота из Set. Вывести на экран всех котов, которые остались во множестве.
+ * <p/>
+ * Date: 07.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        Set<Cat> cats = createCats();
+
+        //Написать тут ваш код
+
+        printCats( cats );
+    }
+
+    public static Set<Cat> createCats()
+    {
+        //Написать тут ваш код
+        return null;
+    }
+
+    public static void printCats( Set<Cat> cats )
+    {
+        for ( Cat cat : cats )
+        {
+            System.out.println( cat );
+        }
+    }
+
+    public static class Cat
+    {
+
+    }
+
+
+}
