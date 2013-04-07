b586cfc37f51baaed0128ee17f4549e5cda62fab
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-07 21:50:11 +0300

    com.javarush.test.level06.lesson11.home04 changed.

--- com/javarush/test/level06/lesson11/home04/Solution.java
+++ com/javarush/test/level06/lesson11/home04/Solution.java
@@ -1,14 +1,31 @@
 package com.javarush.test.level06.lesson11.home04;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
- * User: General
- * Date: 12/21/12
- * Time: 11:59 PM
+ * JavaRush.ru
+ * Level 06, Lesson 11, Home 04
+ * <p/>
+ * Если сегодня четное число, то вывести на экран текст "Мама мыла раму". Если сегодня нечетное число, то вывести на
+ * экран текст "Рама мыла маму".
+ * <p/>
+ * Date: 07.04.13
+ * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        //Напишите тут ваш код
+        GregorianCalendar date = new GregorianCalendar();
+
+        if ( date.get( Calendar.DAY_OF_MONTH ) % 2 == 0 )
+        {
+            System.out.println( "Мама мыла раму" );
+        }
+        else
+        {
+            System.out.println( "Рама мыла маму" );
+        }
     }
 }
