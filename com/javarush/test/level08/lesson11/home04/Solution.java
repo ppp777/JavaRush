ffea8c14588d558dc31bd252b038ae3bb5110781
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-08 19:32:42 +0300

    com.javarush.test.level08.lesson11.home04 added.

---
+++ com/javarush/test/level08/lesson11/home04/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level08.lesson11.home04;
+
+/**
+ * JavaRush.ru
+ * Level 08, Lesson 11, Home 04
+ * <p/>
+ * Ввести с клавиатуры пять чисел. Вывести на экран минимальное из них.
+ * <p/>
+ * Date: 08.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        //Напишите тут ваш код
+    }
+}
