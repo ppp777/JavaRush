fcecb94d00950c540a0e398569b11a546471118d
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-08 19:38:05 +0300

    com.javarush.test.level08.lesson11.home05 added.

---
+++ com/javarush/test/level08/lesson11/home05/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level08.lesson11.home05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * JavaRush.ru
+ * Level 08, Lesson 11, Home 05
+ * <p/>
+ * Написать программу, которая вводит с клавиатуры строку текста. Программа заменяет в тексте первые буквы всех слов на
+ * заглавные.
+ * Пример ввода:
+ * Мама мыла раму.
+ * Пример вывода:
+ * Мама Мыла Раму.
+ * <p/>
+ * Date: 08.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args ) throws IOException
+    {
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        String s = reader.readLine();
+
+        //Напишите тут ваш код
+    }
+}
