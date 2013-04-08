5342efa66df9e5dfacc2158e82737fe8f504dfbe
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-08 19:49:25 +0300

    com.javarush.test.level08.lesson11.home07 added.

---
+++ com/javarush/test/level08/lesson11/home07/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.test.level08.lesson11.home07;
+
+/**
+ * JavaRush.ru
+ * Level 08, Lesson 11, Home 07
+ * <p/>
+ * Переставь один модификатор static так, чтобы пример скомпилировался.
+ * <p/>
+ * Date: 08.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public int A = 5;
+    public static int B = 2;
+
+    public int C = A * B;
+    public static int D = B * A;
+
+    public static void main( String[] args )
+    {
+    }
+
+    public static int getValue()
+    {
+        return D;
+    }
+
+    public int getValue2()
+    {
+        return C;
+    }
+}
+
+
+//Исходный текст программы:
+//public class Solution
+//{
+//    public int A = 5;
+//    public static int B = 2;
+//
+//    public int C = A * B;
+//    public static int D = B * A;
+//
+//    public static void main( String[] args )
+//    {
+//    }
+//
+//    public static int getValue()
+//    {
+//        return D;
+//    }
+//
+//    public int getValue2()
+//    {
+//        return C;
+//    }
+//}
