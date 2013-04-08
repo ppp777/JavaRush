a1cae996c6a290952367755f1fa8837eea821f4a
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-08 21:13:12 +0300

    com.javarush.test.level09.lesson02.task05 added.

---
+++ com/javarush/test/level09/lesson02/task05/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level09.lesson02.task05;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 02, Task 05
+ * <p/>
+ * Написать функцию, которая возвращает результат – глубину её стек трейса – количество функций в нем
+ * (количество элементов в списке). Это же число функция должна выводить на экран.
+ * <p/>
+ * Date: 08.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static int getStackTraceDeep()
+    {
+        //Напишите тут ваш код
+
+    }
+}
