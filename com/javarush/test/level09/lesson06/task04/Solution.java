d32ca6c3f99b46df3cb126e9a4a3bdac26f3623d
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:19:05 +0300

    com.javarush.test.level09.lesson06.task04 added.

---
+++ com/javarush/test/level09/lesson06/task04/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level09.lesson06.task04;
+
+import java.util.ArrayList;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 06, Task 04
+ * <p/>
+ * Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода: <br />
+ * ArrayList<String> list = new ArrayList<String>(); <br />
+ * String s = list.get(18); <br />
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
+    {
+        //Напишите тут ваш код
+
+        ArrayList<String> list = new ArrayList<String>();
+        String s = list.get( 18 );
+
+//Напишите тут ваш код
+
+    }
+}
