53e0fcd3ee4b44e84cf6195e322f0d79ffa785c5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 18:34:07 +0300

    com.javarush.test.level08.lesson11.home06 changed.

--- com/javarush/test/level08/lesson11/home06/Solution.java
+++ com/javarush/test/level08/lesson11/home06/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level08.lesson11.home06;
 
+import java.util.ArrayList;
+
 /**
  * JavaRush.ru
  * Level 08, Lesson 11, Home 06
@@ -12,14 +14,29 @@ package com.javarush.test.level08.lesson11.home06;
  */
 public class Solution
 {
-    public static void main( String[] args )
-    {
-        //Написать тут ваш код
-    }
-
     public static class Human
     {
-        //Написать тут ваш код
+        public static final boolean MALE = true;
+        public static final boolean FEMALE = false;
+
+        private String name;
+        private boolean sex;
+        private int age;
+        private ArrayList<Human> children;
+
+        public Human( String name, boolean sex, int age )
+        {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.children = new ArrayList<Human>();
+        }
+
+        public Human( String name, boolean sex, int age, ArrayList<Human> children )
+        {
+            this( name, sex, age );
+            this.children.addAll( children );
+        }
 
         public String toString()
         {
@@ -41,6 +58,40 @@ public class Solution
             }
 
             return text;
+        }
+    }
+
+    public static void main( String[] args )
+    {
+        Human[] family = new Human[9];
+        ArrayList<Human> childList = new ArrayList<Human>();
+
+        family[0] = new Human( "Татьяна", Human.FEMALE, 16 );
+        family[1] = new Human( "Вовочка", Human.MALE, 10 );
+        family[2] = new Human( "Настя", Human.FEMALE, 5 );
+
+        childList.add( family[0] );
+        childList.add( family[1] );
+        childList.add( family[2] );
+
+        family[3] = new Human( "Veronika Ivanova", Human.FEMALE, 36, childList );
+        family[4] = new Human( "Alexandr Ivanov", Human.MALE, 40, childList );
+
+        childList = new ArrayList<Human>();
+        childList.add( family[4] );
+
+        family[5] = new Human( "Иосиф Виссарионович", Human.MALE, 92, childList );
+        family[6] = new Human( "Изольда Тихоновна", Human.FEMALE, 79, childList );
+
+        childList = new ArrayList<Human>();
+        childList.add( family[3] );
+
+        family[7] = new Human( "Борим Моисеевич", Human.MALE, 69, childList );
+        family[8] = new Human( "Клара Захаровна", Human.FEMALE, 58, childList );
+
+        for ( Human x : family )
+        {
+            System.out.println( x );
         }
     }
 
