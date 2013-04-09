3473855e56d9b968e1c620268a93529861ac0cd9
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:24:56 +0300

    com.javarush.test.level09.lesson06.task02 added.

---
+++ com/javarush/test/level09/lesson06/task02/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level09.lesson06.task02;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 06, Task 02
+ * <p/>
+ * Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода: <br />
+ * String s = null; <br />
+ * String m = s.toLowerCase(); <br />
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Напишите тут ваш код
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //Напишите тут ваш код
+
+    }
+}
