9c77dde71f1f037565e55883eaf518d9cd5ab377
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:27:52 +0300

    com.javarush.test.level09.lesson06.task01 added.

---
+++ com/javarush/test/level09/lesson06/task01/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level09.lesson06.task01;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 06, Task 01
+ * <p/>
+ * Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода: <br />
+ * int a = 42 / 0; <br />
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
+    {
+        //Напишите тут ваш код
+
+        int a = 42 / 0;
+
+        //Напишите тут ваш код
+
+    }
+}
