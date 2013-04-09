11c1e5889f75aeca8fce4d913d3ff98bb01d68a4
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:01:41 +0300

    com.javarush.test.level09.lesson08.task04 added.

---
+++ com/javarush/test/level09/lesson08/task04/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level09.lesson08.task04;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 08, Task 04
+ * <p/>
+ * Создать словарь (Map<String, Date>), и занести в него десять записей по принципу: «фамилия» - «дата рождения».
+ * Удалить из словаря всех людей родившихся летом.
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+//public class Solution
+//{
+//    public static void main( String[] args ) throws Exception
+//    {
+//        //Напишите тут ваш код
+//
+//        method1();
+//
+//        //Напишите тут ваш код
+//
+//    }
+//
+//    public static void method1() throws Exception1, Exception2, Exception3
+//    {
+//        int i = ( int ) ( Math.random() * 3 );
+//        if ( i == 0 )
+//        {
+//            throw new Exception1();
+//        }
+//        if ( i == 1 )
+//        {
+//            throw new Exception2();
+//        }
+//        if ( i == 2 )
+//        {
+//            throw new Exception3();
+//        }
+//    }
+//}
