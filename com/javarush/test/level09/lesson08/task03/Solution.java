a839e5a32794a2409d3c9b3934ecde074b8e023f
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:04:54 +0300

    com.javarush.test.level09.lesson08.task03 added.

---
+++ com/javarush/test/level09/lesson08/task03/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level09.lesson08.task03;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 08, Task 03
+ * <p/>
+ * Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия»- «Имя».
+ * Проверить сколько людей имеют совпадающие с заданным имя или фамилию.
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+//public class Solution
+//{
+//    public static void main( String[] args ) throws Exception
+//    {
+//        //Напишите тут ваш код
+//
+//        method1();
+//
+//        //Напишите тут ваш код
+//
+//    }
+//
+//    public static void method1() throws Exception1, Exception2, Exception3
+//    {
+//        int i = ( int ) ( Math.random() * 3 );
+//        if ( i == 0 )
+//        {
+//            throw new Exception1();
+//        }
+//        if ( i == 1 )
+//        {
+//            throw new Exception2();
+//        }
+//        if ( i == 2 )
+//        {
+//            throw new Exception3();
+//        }
+//    }
+//}
