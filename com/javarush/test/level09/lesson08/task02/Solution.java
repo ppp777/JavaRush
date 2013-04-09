321602bfc1d26258adabff2d10f63d3908f59d58
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:07:10 +0300

    com.javarush.test.level09.lesson08.task02 added.

---
+++ com/javarush/test/level09/lesson08/task02/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level09.lesson08.task02;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 08, Task 02
+ * <p/>
+ * Создать множество чисел(Set<Integer>), занести туда 20 различных чисел. Удалить из множества все числа больше 10.
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+//public class Solution
+//{
+//    public static void main( String[] args ) throws Exception
+//    {
+//        //Напишите тут ваш код
+//
+//        method1();
+//
+//        //Напишите тут ваш код
+//
+//    }
+//
+//    public static void method1() throws Exception1, Exception2, Exception3
+//    {
+//        int i = ( int ) ( Math.random() * 3 );
+//        if ( i == 0 )
+//        {
+//            throw new Exception1();
+//        }
+//        if ( i == 1 )
+//        {
+//            throw new Exception2();
+//        }
+//        if ( i == 2 )
+//        {
+//            throw new Exception3();
+//        }
+//    }
+//}
