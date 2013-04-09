a4bf7fb36da70cb3c8b58673bb4a7935682fc3c5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 18:34:37 +0300

    com.javarush.test.level08.lesson11.home08 changed.

--- com/javarush/test/level08/lesson11/home08/Solution.java
+++ com/javarush/test/level08/lesson11/home08/Solution.java
@@ -34,6 +34,17 @@ public class Solution
 
     public static void sort( int[] array )
     {
-        //Напишите тут ваш код
+        for ( int i = 0; i < array.length - 1; i++ )
+        {
+            for ( int j = 0; j < array.length - i - 1; j++ )
+            {
+                if ( array[j] < array[j + 1] )
+                {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
