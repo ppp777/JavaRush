309b98adcbe55a9633084bec3ee4c047fb3ceefb
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 20:44:56 +0300

    com.javarush.test.level10.lesson02.task01 added.

---
+++ com/javarush/test/level10/lesson04/task01/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level10.lesson04.task01;
+
+/**
+ * JavaRush.ru
+ * Level 10, Lesson 04, Task 05
+ * <p/>
+ * Расставь где нужно оператор приведения типа:<br />
+ * byte a = 1234;<br />
+ * int b = a;<br />
+ * byte c = a * a;<br />
+ * int d = a / c;<br />
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        byte a = ( byte ) 1234;
+        int b = a;
+        byte c = ( byte ) ( a * a );
+        int d = a / c;
+    }
+}
