0d164680622ddf564d9f431e0041f5bde5cf9e0b
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-09 21:22:33 +0300

    com.javarush.test.level09.lesson06.task03 added.

---
+++ com/javarush/test/level09/lesson06/task03/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level09.lesson06.task03;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 06, Task 03
+ * <p/>
+ * Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода: <br />
+ * int[] m = new int[2]; <br />
+ * m[8] = 5; <br />
+ * <p/>
+ * Date: 09.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
+    {
+        //Напишите тут ваш код
+
+        int[] m = new int[2];
+        m[8] = 5;
+
+        //Напишите тут ваш код
+
+    }
+}
