c2bf5d74747bd2f3064c9e4c97ae2298ae2d6ff5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-10 21:24:00 +0300

    com.javarush.test.level08.lesson11.home04 changed.

--- com/javarush/test/level08/lesson11/home04/Solution.java
+++ com/javarush/test/level08/lesson11/home04/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level08.lesson11.home04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * JavaRush.ru
  * Level 08, Lesson 11, Home 04
@@ -11,8 +15,21 @@ package com.javarush.test.level08.lesson11.home04;
  */
 public class Solution
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        //Напишите тут ваш код
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        int minNumber = Integer.parseInt( reader.readLine() );
+
+        for ( int i = 0; i < 4; i++ )
+        {
+            int n = Integer.parseInt( reader.readLine() );
+
+            if ( n < minNumber )
+            {
+                minNumber = n;
+            }
+        }
+
+        System.out.println( minNumber );
     }
 }
