d552d1fbf53fb4bef977a9687c71ad9fea79e0fd
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-10 18:45:54 +0300

    com.javarush.test.level07.lesson06.task01 changed.

--- com/javarush/test/level07/lesson06/task01/Solution.java
+++ com/javarush/test/level07/lesson06/task01/Solution.java
@@ -1,42 +1,32 @@
-/**
- *
- *JavaRush.ru
- *Level 07, Lesson 06, task 01
- *
- * Создай список строк. Добавь в него 5 различных строчек и выведи его размер и содержимое на экран.
- *
- * Date: 28.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level07.lesson06.task01;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+/**
+ * JavaRush.ru
+ * Level 07, Lesson 06, task 01
+ * <p/>
+ * Создай список строк. Добавь в него 5 различных строчек и выведи его размер и содержимое на экран.
+ * <p/>
+ * Date: 28.01.13
+ * @author Sergey Kandalintsev
+ */
 
 public class Solution
 {
     final static int LINES_QTY = 5; //Кол-во вводимых строк
 
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
         ArrayList<String> list = new ArrayList<String>();
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
 
-        for (int i = 0; i < LINES_QTY; i++)
+        for ( int i = 0; i < LINES_QTY; i++ )
         {
-            list.add( br.readLine() );
+            list.add( "String" + i );
         }
 
         System.out.println( list.size() );
-        System.out.println(list);
-
-//        for (String s : list)
-//        {
-//            System.out.println(s);
-//        }
+        System.out.println( list );
     }
 }
 
