6f27826cee392e3a69a80e4afcf58497000de783
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-11 20:56:03 +0300

    com.javarush.test.level09.lesson02.task04 changed.

--- com/javarush/test/level09/lesson02/task04/Solution.java
+++ com/javarush/test/level09/lesson02/task04/Solution.java
@@ -9,67 +9,60 @@ package com.javarush.test.level09.lesson02.task04;
 // * Date: 08.04.13
 // * @author Sergey Kandalintsev
 // */
-//public class Solution
-//{
-//    public static void main( String[] args ) throws Exception
-//    {
-//        method1();
-//    }
-//
-//    public static StackTraceElement[] method1()
-//    {
-//        return method2();
-//    }
-//
-//    public static StackTraceElement[] method2()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method3()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method4()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method5()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method6()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method7()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method8()
-//    {
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static StackTraceElement[] method9()
-//    {
-//        return method10();
-//    }
-//
-//    public static StackTraceElement[] method10()
-//    {
-//        return Thread.currentThread().getStackTrace();
-//    }
-//}
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
+    {
+        method1();
+    }
+
+    public static StackTraceElement[] method1()
+    {
+        return method2();
+    }
+
+    public static StackTraceElement[] method2()
+    {
+        return method3();
+    }
+
+    public static StackTraceElement[] method3()
+    {
+        return method4();
+    }
+
+    public static StackTraceElement[] method4()
+    {
+        return method5();
+    }
+
+    public static StackTraceElement[] method5()
+    {
+        return method6();
+    }
+
+    public static StackTraceElement[] method6()
+    {
+        return method7();
+    }
+
+    public static StackTraceElement[] method7()
+    {
+        return method8();
+    }
+
+    public static StackTraceElement[] method8()
+    {
+        return method9();
+    }
+
+    public static StackTraceElement[] method9()
+    {
+        return method10();
+    }
+
+    public static StackTraceElement[] method10()
+    {
+        return Thread.currentThread().getStackTrace();
+    }
+}
