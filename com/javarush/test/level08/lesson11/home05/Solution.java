7d3285733d750de5118559da56fbf2fb97cecd12
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-12 20:24:43 +0300

    com.javarush.test.level08.lesson11.home05 changed.

--- com/javarush/test/level08/lesson11/home05/Solution.java
+++ com/javarush/test/level08/lesson11/home05/Solution.java
@@ -24,18 +24,38 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
         String s = reader.readLine();
-        String[] splittedWords = s.split( "\\s+" ); // '\s+' - regex for all whitespaces
-
+        char[] letters = s.toCharArray();
         s = "";
-        if ( splittedWords[0].length() > 0 )
-        {
-            for ( String word : splittedWords )
-            {
-                s += word.substring( 0, 1 ).toUpperCase() + word.substring( 1 ) + " ";
-            }
-        }
 
-        s = s.trim();
-        System.out.println( s );
+        int i = 0;
+        while ( letters[i++] != ' ' && i < letters.length )
+        {
+            char ch = Character.toUpperCase( letters[i] );
+
+        }
     }
 }
+
+
+// Альтернативный вариант:
+//public class Solution
+//{
+//    public static void main( String[] args ) throws IOException
+//    {
+//        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+//        String s = reader.readLine();
+//        String[] splittedWords = s.split( "\\s+" ); // '\s+' - regex for all whitespaces
+//
+//        s = "";
+//        if ( splittedWords[0].length() > 0 )
+//        {
+//            for ( String word : splittedWords )
+//            {
+//                s += word.substring( 0, 1 ).toUpperCase() + word.substring( 1 ) + " ";
+//            }
+//        }
+//
+//        s = s.trim();
+//        System.out.println( s );
+//    }
+//}
