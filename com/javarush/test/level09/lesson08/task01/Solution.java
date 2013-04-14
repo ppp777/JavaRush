402f78fe75b5e536f6962aca265ae67f00d14bf1
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-14 21:55:50 +0300

    com.javarush.test.level09.lesson08.task01 changed.

--- com/javarush/test/level09/lesson08/task01/Solution.java
+++ com/javarush/test/level09/lesson08/task01/Solution.java
@@ -2,13 +2,15 @@ package com.javarush.test.level09.lesson08.task01;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 
 /**
  * JavaRush.ru
  * Level 09, Lesson 08, Task 01
  * <p/>
- * Создать множество строк (Set<String>), занести в него 20 слов на букву «Л».
+ * Есть метод, который выбрасывает два исключения унаследованные от Exception и два унаследованных от RuntimeException:
+ * NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException.
+ * Нужно перехватить NullPointerException и FileNotFoundException, но не перехвватывать ArithmeticException и
+ * URISyntaxException. Как это сделать?
  * <p/>
  * Date: 09.04.13
  * @author Sergey Kandalintsev
@@ -18,15 +20,22 @@ public class Solution
 {
     public static void main( String[] args ) throws Exception
     {
-        //Напишите тут ваш код
-
-        method1();
-
-        //Напишите тут ваш код
-
+        try
+        {
+            method1();
+        }
+        catch ( NullPointerException e )
+        {
+            System.out.println( e );
+        }
+        catch ( FileNotFoundException e )
+        {
+            System.out.println( e );
+        }
     }
 
-    public static void method1() throws NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException
+    public static void method1()
+            throws NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException
     {
         int i = ( int ) ( Math.random() * 4 );
         if ( i == 0 )
