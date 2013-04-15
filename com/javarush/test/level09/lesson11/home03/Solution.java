53e3dbccbae36fecf616df701fad242dc2359f03
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-15 20:30:04 +0300

    com.javarush.test.level09.lesson11.home03 added.

---
+++ com/javarush/test/level09/lesson11/home03/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level09.lesson11.home03;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 11, Home 03
+ * <p/>
+ * Вводить с клавиатуры числа. Код по чтению чисел с клавиатуры вынести в отдельный метод. Обернуть все тело (весь
+ * код) этого метода в try..catch. Если пользователь ввел какой-то текст, вместо ввода числа, функция должна
+ * перехватить исключение и вывести все введенные числа в качестве результата.
+ * <p/>
+ * Date: 15.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        readData();
+    }
+
+    public static void readData()
+    {
+        //add your code here
+    }
+}
