376107d18d45a6b504603d4e34ae45cce7cb967b
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-15 20:22:29 +0300

    com.javarush.test.level09.lesson11.home02 added.

--- com/javarush/test/level09/lesson11/home02/Solution.java
+++ com/javarush/test/level09/lesson11/home02/Solution.java
@@ -7,24 +7,25 @@ package com.javarush.test.level09.lesson11.home02;
  * Написать в цикле обратный отсчет от 10 до 0. Для задержки использовать Thread.sleep(1000);
  * Обернуть вызов sleep в try..catch.
  * <p/>
- * Date: 11.04.13
+ * Date: 15.04.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
     public static void main( String[] args )
     {
-        try
+        for ( int i = 10; i >= 0; i-- )
         {
-            for ( int i = 10; i >= 0; i-- )
+            System.out.println( i );
+            try
             {
-                System.out.println( i );
                 Thread.sleep( 1000 );
             }
-        }
-        catch ( InterruptedException e )
-        {
-            e.printStackTrace();
+            catch ( InterruptedException e )
+            {
+                System.out.println( e );
+
+            }
         }
     }
 }
