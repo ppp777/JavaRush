a3bf1a38e2d763d58dfcabcc41817bd0563fb4d5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-15 19:42:27 +0300

    com.javarush.test.level09.lesson08.task05 task04 changed.

--- com/javarush/test/level09/lesson08/task05/Solution.java
+++ com/javarush/test/level09/lesson08/task05/Solution.java
@@ -4,32 +4,54 @@ package com.javarush.test.level09.lesson08.task05;
  * JavaRush.ru
  * Level 09, Lesson 08, Task 05
  * <p/>
- * Создать словарь (Map<String, String>) занести в него десять записей по принципу «фамилия»- «имя».
- * Удалить людей, имеющих одинаковые имена.
+ * То же задание что и level09.lesson08.task02, но: <b />
+ * Напиши catch, который перехватит Exception2, Exception3, но не Exception1. <b />
+ * Подсказка:<b />
  * <p/>
- * Date: 09.04.13
+ * <p/>
+ * Date: 15.04.13
  * @author Sergey Kandalintsev
  */
-//public class Solution
-//{
-//    public static void main(String[] args) throws Exception
-//    {
-//        //Напишите тут ваш код
-//
-//        method1();
-//
-//        //Напишите тут ваш код
-//
-//    }
-//
-//    public static void method1() throws Exception1, Exception2, Exception3
-//    {
-//        int i = (int) (Math.random() * 3);
-//        if (i == 0)
-//            throw new Exception1();
-//        if (i == 1)
-//            throw new Exception2();
-//        if (i == 2)
-//            throw new Exception3();
-//    }
-//}
+public class Solution
+{
+    static class Exception1 extends Exception
+    {
+    }
+
+    static class Exception2 extends Exception1
+    {
+    }
+
+    static class Exception3 extends Exception2
+    {
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        try
+        {
+            method1();
+        }
+        catch ( Exception2 e )
+        {
+            System.out.println( e );
+        }
+    }
+
+    public static void method1() throws Exception1, Exception2, Exception3
+    {
+        int i = ( int ) ( Math.random() * 3 );
+        if ( i == 0 )
+        {
+            throw new Exception1();
+        }
+        if ( i == 1 )
+        {
+            throw new Exception2();
+        }
+        if ( i == 2 )
+        {
+            throw new Exception3();
+        }
+    }
+}
