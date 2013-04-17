160f047c6a427bbf0f44120965bc3eaade6011d6
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-17 18:06:13 +0300

    com.javarush.test.level09.lesson11.home07 changed.

---
+++ com/javarush/test/level09/lesson11/home07/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level09.lesson11.home07;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 11, Home 07
+ * <p/>
+ * Расставь модификаторы static так, чтобы пример скомпилировался.
+ * <p/>
+ * Date: 18.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public int A = 5;
+    public int B = 2 * A;
+    public int C = A * B;
+    public int D = A * B;
+
+    public void main( String[] args )
+    {
+        Solution room = new Solution();
+        room.A = 5;
+
+        Solution.D = 5;
+    }
+
+    public int getA()
+    {
+        return A;
+    }
+
+}
