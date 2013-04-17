63838fa25d975b44067e2ff6ecdae0cd72604bba
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-17 18:57:30 +0300

    com.javarush.test.level09.lesson11.home08 changed.

---
+++ com/javarush/test/level09/lesson11/home08/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.test.level09.lesson11.home08;
+
+import java.util.ArrayList;
+
+/**
+ * JavaRush.ru
+ * Level 09, Lesson 11, Home 08
+ * <p/>
+ * Создать список, элементами которого будут массивы чисел. Добавить в список пять объектов-массивов длиной
+ * 5, 2, 4, 7, 0 соответственно. Заполнить массивы любыми данными и вывести их на экран.
+ * <p/>
+ * Date: 18.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        ArrayList<int[]> list = createList();
+        printList( list );
+    }
+
+    public static ArrayList<int[]> createList()
+    {
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        list.add( new int[5] );
+        list.add( new int[2] );
+        list.add( new int[4] );
+        list.add( new int[7] );
+        list.add( new int[0] );
+
+        for ( int[] array : list )
+        {
+            for ( int i = 0; i < array.length; i++ )
+            {
+                array[i] = i;
+            }
+        }
+
+        return list;
+    }
+
+    public static void printList( ArrayList<int[]> list )
+    {
+        for ( int[] array : list )
+        {
+            for ( int x : array )
+            {
+                System.out.println( x );
+            }
+        }
+    }
+}
