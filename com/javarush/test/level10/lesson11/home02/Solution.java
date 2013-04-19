8e43fa099ef5a36776561a8dc746e5560542dd7e
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-19 18:43:45 +0300

    com.javarush.test.level10.lesson11.home02 added.

---
+++ com/javarush/test/level10/lesson11/home02/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level10.lesson11.home02;
+
+/**
+ * User: General
+ * Date: 12/21/12
+ * Time: 11:59 PM
+ */
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b/c/e;
+        System.out.println(d);
+    }
+}
