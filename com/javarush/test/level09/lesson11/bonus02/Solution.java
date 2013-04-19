cf4d5fe6fdab7c3a0651578ea8df6f4bdf88e48c
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-19 18:17:57 +0300

    com.javarush.test.level09.lesson11.bonus02 changed.

--- com/javarush/test/level09/lesson11/bonus02/Solution.java
+++ com/javarush/test/level09/lesson11/bonus02/Solution.java
@@ -3,29 +3,78 @@ package com.javarush.test.level09.lesson11.bonus02;
 import java.io.*;
 
 /**
- * User: General
- * Date: 12/21/12
- * Time: 11:59 PM
+ * JavaRush.ru
+ * Level 09, Lesson 11, Bonus 02
+ * <p/>
+ * Нужно добавить в программу новую функциональность.<b/>
+ * Задача: <b/>
+ * Программа вводит два имени файла. И копирует первый файл на место заданное вторым именем. <b/>
+ * Новая задача: <b/>
+ * Программа вводит два имени файла. И копирует первый файл на место заданное вторым именем. Если файла (который нужно
+ * копировать) с указанным именем не существует, программа должна вывести надпись "Файл не существует." и еще раз
+ * прочитать имя файла с консоли. <b/>
+ * <p/>
+ * Date: 19.04.13
+ * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args) throws IOException
+    public static void main( String[] args ) throws IOException
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        String sourceFileName;
+        String destinationFileName;
+        FileInputStream fileInputStream;
 
-        String sourceFileName = reader.readLine();
-        String destinationFileName = reader.readLine();
+        while ( true )
+        {
+            sourceFileName = reader.readLine();
 
-        FileInputStream fileInputStream = new FileInputStream(sourceFileName);
-        FileOutputStream fileOutputStream = new FileOutputStream(destinationFileName);
+            try
+            {
+                fileInputStream = new FileInputStream( sourceFileName );
+                break;
+            }
+            catch ( FileNotFoundException e )
+            {
+                System.out.println( "Файл не существует." );
+            }
+        }
 
-        while (fileInputStream.available() > 0)
+        destinationFileName = reader.readLine();
+        FileOutputStream fileOutputStream = new FileOutputStream( destinationFileName );
+
+        while ( fileInputStream.available() > 0 )
         {
             int data = fileInputStream.read();
-            fileOutputStream.write(data);
+            fileOutputStream.write( data );
         }
 
         fileInputStream.close();
         fileOutputStream.close();
     }
 }
+
+//Исходный вариант:
+//public class Solution
+//{
+//    public static void main( String[] args ) throws IOException
+//    {
+//        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+//
+//        String sourceFileName = reader.readLine();
+//        String destinationFileName = reader.readLine();
+//
+//        FileInputStream fileInputStream = new FileInputStream( sourceFileName );
+//        FileOutputStream fileOutputStream = new FileOutputStream( destinationFileName );
+//
+//        while ( fileInputStream.available() > 0 )
+//        {
+//            int data = fileInputStream.read();
+//            fileOutputStream.write( data );
+//        }
+//
+//        fileInputStream.close();
+//        fileOutputStream.close();
+//    }
+//}
