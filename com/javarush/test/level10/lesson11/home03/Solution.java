0902cd9eb59aaaf8cbd47c74478e8880c5a01a47
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-19 19:11:42 +0300

    com.javarush.test.level10.lesson11.home03 added.

--- com/javarush/test/level10/lesson11/home03/Solution.java
+++ com/javarush/test/level10/lesson11/home03/Solution.java
@@ -1,20 +1,24 @@
 package com.javarush.test.level10.lesson11.home03;
 
 /**
- * User: General
- * Date: 12/21/12
- * Time: 11:59 PM
+ * JavaRush.ru
+ * Level 10, Lesson 11, Home 03
+ * <p/>
+ * Добавить одну операцию по преобразованию типа, чтобы получался ответ: d = 1.0
+ * <p/>
+ * Date: 19.04.13
+ * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
         int a = 257;
         int b = 4;
         int c = 3;
         int e = 2;
-        double d = a + b/c/e;
+        double d = a + b / c / e;
 
-        System.out.println(d);
+        System.out.println( d );
     }
 }
