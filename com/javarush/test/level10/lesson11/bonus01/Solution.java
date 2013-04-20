f2d00621323ca46b43a0a8a86e2beedf06c3a4ba
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-20 17:06:04 +0300

    com.javarush.test.level10.lesson11.bonus01 changed.

--- com/javarush/test/level10/lesson11/bonus01/Solution.java
+++ com/javarush/test/level10/lesson11/bonus01/Solution.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * JavaRush.ru
  * Level 10, Lesson 11, Bonus 01
  * <p/>
- * Нужно исправить программу, чтобы компилировалась и работала.
+ * Нужно исправить программу, чтобы компилировалась и работала. <b/>
  * Задача: Программа демонстрирует работу HashMap: вводит с клавиатуры набор пар (номер и строку), помещает их в
  * HashMap и выводит на экран содержимое HashMap.
  * <p/>
@@ -40,3 +40,28 @@ public class Solution
         }
     }
 }
+
+// Исходный вариант:
+//public class Solution
+//{
+//    public static void main(String[] args) throws IOException
+//    {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        HashMap<int, String> map = new HashMap<int, String>();
+//
+//        for (int i = 0; i < 10; i++)
+//        {
+//            int index = Integer.parseInt(reader.readLine());
+//            String name = reader.readLine();
+//            map.put(index, name);
+//        }
+//
+//        for (Map.Entry<int, String> pair : map.entrySet())
+//        {
+//            int id = pair.getKey();
+//            String name = pair.getValue();
+//            System.out.println("Id=" + id + " Name=" + name);
+//        }
+//    }
+//}
