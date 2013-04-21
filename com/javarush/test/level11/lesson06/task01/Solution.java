c4a3c156889d6e80f132292211d45fe76e3bc7fc
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-21 19:26:42 +0300

    com.javarush.test.level11.lesson06.task01 added.

---
+++ com/javarush/test/level11/lesson06/task01/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level11.lesson06.task01;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 06, Task 01
+ * <p/>
+ * Написать два класса: Horse (лошадь) и Pegas (пегас). Унаследовать пегаса от лошади.
+ * <p/>
+ * Date: 21.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Horse
+    {
+
+    }
+
+    public class Pegas extends Horse
+    {
+
+    }
+}
+
