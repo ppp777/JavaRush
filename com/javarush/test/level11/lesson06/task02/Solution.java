5d57b542ebb9bcfc0598bc8069317cf1975cd085
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-22 19:23:39 +0300

    com.javarush.test.level11.lesson06.task02 added.

---
+++ com/javarush/test/level11/lesson06/task02/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level11.lesson06.task02;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 06, Task 02
+ * <p/>
+ * Написать три класса: Pet (домашнее животное), Cat(кот) и Dog(собака). Унаследовать кота и собаку от животного.
+ * <p/>
+ * Date: 22.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Pet
+    {
+    }
+
+    public class Cat extends Pet
+    {
+    }
+
+    public class Dog extends Pet
+    {
+    }
+}
+
+
