61f9c0e57906bcd2a9516cd4475f3d64a2e4462f
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-24 21:03:34 +0300

    com.javarush.test.level11.lesson06.task04 added.

---
+++ com/javarush/test/level11/lesson06/task04/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level11.lesson06.task04;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 06, Task 04
+ * <p/>
+ * Написать четыре класса: Worker(сотрудник), Manager(управляющий), Chief(директор) и  Secretary(секретарь).
+ * Унаследовать управляющего, директора и секретаря от сотрудника.
+ * <p/>
+ * Date: 24.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public class Worker
+    {
+    }
+
+    public class Manager extends Worker
+    {
+    }
+
+    public class Chief extends Worker
+    {
+    }
+
+    public class Secretary extends Worker
+    {
+    }
+}
