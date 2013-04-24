f73655f573c6f9c1e9ff9006a853ac982a034f02
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-24 21:00:05 +0300

    com.javarush.test.level11.lesson06.task03 added.

---
+++ com/javarush/test/level11/lesson06/task03/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level11.lesson06.task03;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 06, Task 03
+ * <p/>
+ * Написать шесть классов: Animal (животное), Cow(корова) и Pig(свинья), Sheep(овца), Bull(бык), Goat(козел).
+ * Унаследовать корову, свинью, овцу, быка и козла от животного.
+ * <p/>
+ * Date: 24.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Animal
+    {
+    }
+
+    public class Cow extends Animal
+    {
+    }
+
+    public class Pig extends Animal
+    {
+    }
+
+    public class Sheep extends Animal
+    {
+    }
+
+    public class Bull extends Animal
+    {
+    }
+
+    public class Goat extends Animal
+    {
+    }
+}
