607e7ce3ae9ca9c4221c3c652b5c489cac9b7ff5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-24 19:41:10 +0300

    com.javarush.test.level10.lesson11.home07 added.

---
+++ com/javarush/test/level10/lesson11/home07/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.test.level10.lesson11.home07;
+
+/**
+ * JavaRush.ru
+ * Level 10, Lesson 11, Home 07
+ * <p/>
+ * Расставь модификаторы static так, чтобы пример скомпилировался.
+ * <p/>
+ * Date: 24.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static int A = 5;
+    public static int B = 5;
+    public static int C = 5;
+    public static int D = 5;
+
+    public static void main( String[] args )
+    {
+        Solution solution = new Solution();
+        solution.A = 5;
+        solution.B = 5 * B;
+        solution.C = 5 * C * D;
+        Solution.D = 5 * D * C;
+
+        Solution.D = 5;
+    }
+
+    public int getA()
+    {
+        return A;
+    }
+
+}
+
+//Исходный вариант:
+//public class Solution
+//{
+//    public int A = 5;
+//    public int B = 5;
+//    public int C = 5;
+//    public static int D = 5;
+//
+//    public static void main( String[] args )
+//    {
+//        Solution solution = new Solution();
+//        solution.A = 5;
+//        solution.B = 5 * B;
+//        solution.C = 5 * C * D;
+//        Solution.D = 5 * D * C;
+//
+//        Solution.D = 5;
+//    }
+//
+//    public int getA()
+//    {
+//        return A;
+//    }
+//}
