6fae26fe791263143f1865a6c8fc28e10a116646
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 20:11:52 +0300

    com.javarush.test.level11.lesson11.home10 added.

---
+++ com/javarush/test/level11/lesson11/home10/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level11.lesson11.home10;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 10
+ * <p/>
+ * Расставь правильно "цепочку наследования" в классах: House, Cat, Dog, Car, Animal, Assets.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class House extends Assets
+    {
+    }
+
+    public class Cat extends Animal
+    {
+    }
+
+    public class Car extends Assets
+    {
+    }
+
+    public class Dog extends Animal
+    {
+    }
+
+    public class Animal
+    {
+    }
+
+    public class Assets
+    {
+    }
+}
