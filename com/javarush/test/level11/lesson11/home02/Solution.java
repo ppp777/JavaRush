28b63f921f7ddcf7bfd1328e09700df6fd725a3a
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 19:00:09 +0300

    com.javarush.test.level11.lesson11.home03 added.

--- com/javarush/test/level11/lesson11/home02/Solution.java
+++ com/javarush/test/level11/lesson11/home02/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.test.level11.lesson11.home02;
 
-
 /**
  * JavaRush.ru
  * Level 11, Lesson 11, Home 02
@@ -10,7 +9,6 @@ package com.javarush.test.level11.lesson11.home02;
  * Date: 25.04.13
  * @author Sergey Kandalintsev
  */
-
 public class Solution
 {
     public static void main( String[] args )
---
+++ com/javarush/test/level11/lesson11/home03/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level11.lesson11.home03;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 03
+ * <p/>
+ * Написать четыре класса Fish, Animal, Ape, Human. Унаследовать животных от рыб, обезьян от животных и человека от
+ * обезьяны.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Fish
+    {
+    }
+
+    public class Animal extends Fish
+    {
+    }
+
+    public class Ape extends Animal
+    {
+    }
+
+    public class Human extends Ape
+    {
+    }
+}
