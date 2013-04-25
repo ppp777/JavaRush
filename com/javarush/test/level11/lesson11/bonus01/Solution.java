6736bd2efea8ee0c93a17e47664fb38f639fd453
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 20:17:35 +0300

    com.javarush.test.level11.lesson11.bonus01 added.

---
+++ com/javarush/test/level11/lesson11/bonus01/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level11.lesson11.bonus01;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Bonus 01
+ * <p/>
+ * Нужно исправить программу чтобы компилировалась и работала. <b/>
+ * Исправь наследование в классах:(Cat, Dog, Pet, House, Airplane)
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Pet
+    {
+    }
+
+    public class Cat extends Pet
+    {
+    }
+
+    public class Dog extends Pet
+    {
+    }
+
+    public class House
+    {
+    }
+
+    public class Airplane
+    {
+    }
+}
