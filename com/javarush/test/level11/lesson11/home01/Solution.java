af07bfb6405fb499825166e214836ec84748a0f8
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 18:47:49 +0300

    com.javarush.test.level11.lesson11.home01 added.

---
+++ com/javarush/test/level11/lesson11/home01/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level11.lesson11.home01;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 01
+ * <p/>
+ * Написать два класса Adam и Eve. Унаследовать Еву от Адама.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    //Адам
+    public class Adam
+    {
+    }
+
+    //Ева
+    public class Eve extends Adam
+    {
+    }
+}
