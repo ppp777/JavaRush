48573d8319cd77d8222a9762bdbbcf0fd2326ac9
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 18:51:31 +0300

    com.javarush.test.level11.lesson11.home02 added.

---
+++ com/javarush/test/level11/lesson11/home02/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level11.lesson11.home02;
+
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 02
+ * <p/>
+ * Написать два класса AppleIPhone и SamsungGalaxyS2. Унаследовать SamsungGalaxyS2 от AppleIPhone.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class AppleIPhone
+    {
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone
+    {
+    }
+}
