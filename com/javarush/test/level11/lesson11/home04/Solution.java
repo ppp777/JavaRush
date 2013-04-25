daa146e11dbbe764618f2c8b2616a44adc5290ef
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 19:50:57 +0300

    com.javarush.test.level11.lesson11.home04 added.

---
+++ com/javarush/test/level11/lesson11/home04/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level11.lesson11.home04;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 04
+ * <p/>
+ * Написать три класса Judaism, Christianity, Islam. Унаследовать христианство от иудаизма и мусульманство от
+ * христианства.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Judaism
+    {
+    }
+
+    public class Christianity extends Judaism
+    {
+    }
+
+    public class Islam extends Christianity
+    {
+    }
+}
