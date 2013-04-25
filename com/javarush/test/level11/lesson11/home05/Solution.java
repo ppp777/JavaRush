5c0e9db999b2181d5dcf77500934c54ca007feaa
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 19:55:12 +0300

    com.javarush.test.level11.lesson11.home05 added.

---
+++ com/javarush/test/level11/lesson11/home05/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level11.lesson11.home05;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Home 05
+ * <p/>
+ * Написать четыре класса Schoolboy, Student, Worker, Slave. Унаследовать студента от школьника, сотрудника от студента,
+ * раба от сотрудника.
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class Schoolboy
+    {
+    }
+
+    public class Student extends Schoolboy
+    {
+    }
+
+    public class Worker extends Student
+    {
+    }
+
+    public class Slave extends Worker
+    {
+    }
+}
