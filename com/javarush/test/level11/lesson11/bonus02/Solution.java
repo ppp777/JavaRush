a1c260fa34b634b52cabc9ec02cfa77864a1f205
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-25 20:24:47 +0300

    com.javarush.test.level11.lesson11.bonus02 added.

---
+++ com/javarush/test/level11/lesson11/bonus02/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.test.level11.lesson11.bonus02;
+
+/**
+ * JavaRush.ru
+ * Level 11, Lesson 11, Bonus 02
+ * <p/>
+ * Нужно добавить в программу новую функциональность. <b/>
+ *Добавь общий базовый класс к классам-фигур:(фигуры из шахмат)
+ * <p/>
+ * Date: 25.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public class ChessItem
+    {
+    }
+    public class King extends ChessItem
+    {
+    }
+
+    public class Queen extends ChessItem
+    {
+    }
+
+    public class Rook extends ChessItem
+    {
+    }
+
+    public class Knight extends ChessItem
+    {
+    }
+
+    public class Bishop extends ChessItem
+    {
+    }
+
+    public class Pawn extends ChessItem
+    {
+    }
+}
