833ce2d7d4a65cd91d25b8164449c603a0e85434
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-26 19:48:23 +0300

    com.javarush.test.level12.lesson04.task01 added.

---
+++ com/javarush/test/level12/lesson04/task01/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level12.lesson04.task01;
+
+/**
+ * JavaRush.ru
+ * Level 12, Lesson 04, Task 01
+ * <p/>
+ * Написать два метода: print(int) и print(String).
+ * Date: 26.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        print( 5 );
+        print( "25" );
+    }
+
+    public static void print( int n )
+    {
+        System.out.println( n );
+    }
+
+    public static void print( String s )
+    {
+        System.out.println( s );
+    }
+}
