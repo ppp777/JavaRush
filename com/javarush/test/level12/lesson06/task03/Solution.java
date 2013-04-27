5c302e3705ccf93a1cd59cb00c4425b33dcb1a01
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-04-27 15:21:05 +0300

    com.javarush.test.level12.lesson06.task03 added.

---
+++ com/javarush/test/level12/lesson06/task03/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level12.lesson06.task03;
+
+/**
+ * JavaRush.ru
+ * Level 12, Lesson 06, Bonus 03
+ * <p/>
+ * Исправь код, чтобы программа компилировалась.
+ * <p/>
+ * Date: 27.04.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+
+    public abstract class Pet
+    {
+        public String getName()
+        {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+}
