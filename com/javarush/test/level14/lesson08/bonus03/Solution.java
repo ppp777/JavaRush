f4869628a3178206af183b68166fe39559067178
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-03 17:56:56 +0300

    com.javarush.test.level14.lesson08.bonus03 added.

---
+++ com/javarush/test/level14/lesson08/bonus03/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level14.lesson08.bonus03;
+
+/**
+ * JavaRush.ru
+ * Level 14, Lesson 08, Bonus 03
+ * <p/>
+ * Класс является синглтоном (реализует паттерн(шаблон) Singleton), если позволяет создать всего один объект
+ * своего типа.
+ * Реализовать Singleton pattern:
+ * 1. Создать класс Singleton в отдельном файле. <b/>
+ * 2. Добавить в него статический метод getInstance(). <b/>
+ * 3. Метод getIstance должен возвращать один и тот же объект класса Singleton при любом вызове метода getInstance. <b/>
+ * 4. Подумайте, каким образом можно запретить создание других объектов этого класса. <b/>
+ * 5. Сделайте все конструкторы в классе Singleton приватными (private). <b/>
+ * 6. В итоге должна быть возможность создать объект (экземпляр класса) ТОЛЬКО используя метод getInstance.
+ * <p/>
+ * Date: 03.05.13
+ * @author Sergey Kandalintsev
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+    }
+}
