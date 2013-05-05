ffc1b22209a0be947233904fb74cfc5532bffe09
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-05 18:34:16 +0300

    com.javarush.test.level04.lesson16.home03 changed.

--- com/javarush/test/level04/lesson16/home03/Solution.java
+++ com/javarush/test/level04/lesson16/home03/Solution.java
@@ -1,31 +1,36 @@
 package com.javarush.test.level04.lesson16.home03;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 16, Home 03
- *
+ * JavaRush.ru
+ * Level 04, Lesson 16, Home 03
+ * <p/>
  * Вводить с клавиатуры числа и считать их сумму. Вывести на экран полученную сумму. Если пользователь ввел -1,
  * вывести на экран сумму и завершить программу.
- *
+ * <p/>
  * Date: 22.01.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int  sum = 0;
+    public static void main( String[] args ) throws Exception
+    {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        int sum = 0;
 
-        while (true) {
-            int nNextNumber = Integer.parseInt(br.readLine());
-            sum += nNextNumber;
-            if (nNextNumber == -1)
+        while ( true )
+        {
+            int nextNumber = Integer.parseInt( br.readLine() );
+            sum += nextNumber;
+            if ( nextNumber == -1 )
+            {
                 break;
+            }
         }
 
-        System.out.println(sum);
+        System.out.println( sum );
     }
 }
 
