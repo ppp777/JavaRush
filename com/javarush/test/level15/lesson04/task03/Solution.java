bbf5553e2399a1dc23f81193ac666683207f58ae
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-06 13:52:56 +0300

    com.javarush.test.level15.lesson04.task03 changed.

--- com/javarush/test/level15/lesson04/task03/Solution.java
+++ com/javarush/test/level15/lesson04/task03/Solution.java
@@ -1,33 +1,44 @@
 package com.javarush.test.level15.lesson04.task03;
 
-/*
-1. Подумайте, какая из реализаций метода print будет вызвана.
-2. Удалите все лишние реализации метода print.
+/**
+ * JavaRush.ru
+ * Level 15, Lesson 04, Task 03
+ * <p/>
+ * 1. Подумайте, какая из реализаций метода print будет вызвана. <b/>
+ * 2. Удалите все лишние реализации метода print.
+ * <p/>
+ * Date: 04.05.13
+ * @author Sergey Kandalintsev
  */
-public class Solution {
-    public static void main(String[] args) {
-        print(1);
-    }
-
+public class Solution
+{
+//    public static void main( String[] args )
+//    {
+//        print( 1 );
+//    }
+//
 //    public static void print( byte b )
 //    {
 //        System.out.println( "Я буду Java прогером!" );
 //    }
-//
-//    public static void print( double d )
+
+    public static void print( double d )
+    {
+        System.out.println( "Я буду Java прогером!" );
+    }
+
+    public static void print( long l )
+    {
+        System.out.println( "Я буду Java прогером!" );
+    }
+
+//    public static void print( float f )
 //    {
 //        System.out.println( "Я буду Java прогером!" );
 //    }
-
-    public static void print(long l) {
-        System.out.println("Я буду Java прогером!");
-    }
-
-//    public static void print(float f) {
-//        System.out.println("Я буду Java прогером!");
-//    }
 //
-//    public static void print(char c) {
-//        System.out.println("Я буду Java прогером!");
+//    public static void print( char c )
+//    {
+//        System.out.println( "Я буду Java прогером!" );
 //    }
 }
