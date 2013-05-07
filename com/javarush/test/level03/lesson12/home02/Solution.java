e30b5b6191a467454dfd2799738234181254bc22
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-07 20:07:07 +0300

    com.javarush.test.level03.lesson12.home02 changed.

--- com/javarush/test/level03/lesson12/home02/Solution.java
+++ com/javarush/test/level03/lesson12/home02/Solution.java
@@ -2,9 +2,11 @@ package com.javarush.test.level03.lesson12.home02;
 
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        for (int i = 0; i < 10; i++)
-            System.out.println("Я не хочу изучать Java, я хочу большую зарплату");
+        for ( int i = 0; i < 10; i++ )
+        {
+            System.out.println( "Я не хочу изучать Java, я хочу большую зарплату" );
+        }
     }
 }
