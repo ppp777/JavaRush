3b3921b7b49f46af464a2c2dbc68dddc5a7e713c
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-07 20:07:55 +0300

    com.javarush.test.level03.lesson12.home03 changed.

--- com/javarush/test/level03/lesson12/home03/Solution.java
+++ com/javarush/test/level03/lesson12/home03/Solution.java
@@ -5,11 +5,12 @@ import java.io.InputStreamReader;
 
 public class Solution
 {
-    public static void main(String[] args)   throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main( String[] args ) throws Exception
+    {
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
         String sAmount = reader.readLine();
-        int nAmount = Integer.parseInt(sAmount);
-        System.out.println("Я буду зарабатывать $" + nAmount + " в час");
+        int nAmount = Integer.parseInt( sAmount );
+        System.out.println( "Я буду зарабатывать $" + nAmount + " в час" );
     }
 
 }
