59c0956a4b3c10b18b364dbcdbacbdaa171fe0cf
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-08 21:17:14 +0300

    com.javarush.test.level05.lesson07.task01 changed.

--- com/javarush/test/level05/lesson07/task01/Friend.java
+++ com/javarush/test/level05/lesson07/task01/Friend.java
@@ -1,34 +1,36 @@
 package com.javarush.test.level05.lesson07.task01;
 
 /**
- *JavaRush.ru
- *Level 05, Lesson 07, task 01
- *
- * Создать класс Friend (друг) с тремя инициализаторами (тремя методами initialize):
- * - Имя
- * - Имя, возраст
+ * JavaRush.ru
+ * Level 05, Lesson 07, task 01
+ * <p/>
+ * Создать класс Friend (друг) с тремя инициализаторами (тремя методами initialize): <b/>
+ * - Имя <b/>
+ * - Имя, возраст <b/>
  * - Имя, возраст, пол
- *
+ * <p/>
  * Date: 23.01.13
  * @author Sergey Kandalintsev
  */
-
 public class Friend
 {
     private String name;
     private int age;
     private String gender;
 
-    public void initialize(String name) {
+    public void initialize( String name )
+    {
         this.name = name;
     }
 
-    public void initialize(String name, int age) {
+    public void initialize( String name, int age )
+    {
         this.name = name;
         this.age = age;
     }
 
-    public void initialize(String name, int age, String gender) {
+    public void initialize( String name, int age, String gender )
+    {
         this.name = name;
         this.age = age;
         this.gender = gender;
