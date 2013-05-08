2559a2773e959bf82e60c43ea0349a1a2e461559
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-08 17:26:53 +0300

    com.javarush.test.level04.lesson13.task02 changed.

--- com/javarush/test/level04/lesson13/task02/Solution.java
+++ com/javarush/test/level04/lesson13/task02/Solution.java
@@ -4,26 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 13, Task 02
- *
+ * JavaRush.ru
+ * Level 04, Lesson 13, Task 02
+ * <p/>
  * Ввести с клавиатуры два числа m и n. Вывести на экран прямоугольник размером m на n из восьмёрок(‘8’)
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String firstNumber = br.readLine();
-        String secondNumber = br.readLine();
-        int m = Integer.parseInt(firstNumber);
-        int n = Integer.parseInt(secondNumber);
+    public static void main( String[] args ) throws Exception
+    {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++)
-                System.out.print("8");
+        int m = Integer.parseInt( br.readLine() );
+        int n = Integer.parseInt( br.readLine() );
+
+        for ( int i = 0; i < m; i++ )
+        {
+            for ( int j = 0; j < n; j++ )
+            {
+                System.out.print( "8" );
+            }
             System.out.println();
         }
     }
