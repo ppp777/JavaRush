5718b559a84ace9ffb67fcb057fec3276fff899a
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-08 21:20:11 +0300

    com.javarush.test.level05.lesson07.task02 changed.

--- com/javarush/test/level05/lesson07/task02/Cat.java
+++ com/javarush/test/level05/lesson07/task02/Cat.java
@@ -1,23 +1,22 @@
+package com.javarush.test.level05.lesson07.task02;
+
 /**
- *JavaRush.ru
- *Level 05, Lesson 07, task 02
- *
- * Создать класс Cat (кот) с пятью инициализаторами:
- *  - Имя,
- *  - Имя, вес, возраст
- *  - Имя, возраст (вес стандартный)
- *  - вес, цвет, (имя, адрес и возраст неизвестны, это бездомный кот)
- *  - вес, цвет, адрес ( чужой домашний кот)
- *  Задача инициализатора – сделать объект валидным. Например, если вес неизвестен, то нужно указать какой-нибудь
- *  средний вес. Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null).
- *  То же касается адреса: null.
- *
+ * JavaRush.ru
+ * Level 05, Lesson 07, task 02
+ * <p/>
+ * Создать класс Cat (кот) с пятью инициализаторами: <b/>
+ * - Имя, <b/>
+ * - Имя, вес, возраст <b/>
+ * - Имя, возраст (вес стандартный) <b/>
+ * - вес, цвет, (имя, адрес и возраст неизвестны, это бездомный кот) <b/>
+ * - вес, цвет, адрес ( чужой домашний кот) <b/>
+ * Задача инициализатора – сделать объект валидным. Например, если вес неизвестен, то нужно указать какой-нибудь
+ * средний вес. Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null).
+ * То же касается адреса: null.
+ * <p/>
  * Date: 23.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level05.lesson07.task02;
-
 public class Cat
 {
     private String name;
@@ -26,27 +25,36 @@ public class Cat
     private String color;
     private int weight;
 
-    public void initialize(String name) {
+    public void initialize( String name )
+    {
         this.name = name;
         this.age = 2;
         this.weight = 3;
     }
-    public void initialize(String name, int weight, int age) {
+
+    public void initialize( String name, int weight, int age )
+    {
         this.name = name;
         this.weight = weight;
         this.age = age;
     }
-    public void initialize(String name, int age) {
+
+    public void initialize( String name, int age )
+    {
         this.name = name;
         this.age = age;
         this.weight = 3;
     }
-    public void initialize(int weight, String color) {
+
+    public void initialize( int weight, String color )
+    {
         this.weight = weight;
         this.color = color;
         this.age = 2;
     }
-    public void initialize(int weight, String color, String address) {
+
+    public void initialize( int weight, String color, String address )
+    {
         this.weight = weight;
         this.color = color;
         this.address = address;
