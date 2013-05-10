4531a1735aff591e7fc4be11225f76928d20018a
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-10 10:00:28 +0300

    com.javarush.test.level07.lesson09.task05 changed.

--- com/javarush/test/level07/lesson09/task05/Solution.java
+++ com/javarush/test/level07/lesson09/task05/Solution.java
@@ -1,40 +1,37 @@
-/**
- *
- *JavaRush.ru
- *Level 07, Lesson 09, task 05
- *
- * Введи с клавиатуры 10 слов в массив строк. Удвой их по принципу a,b,c -> a,a,b,b,c,c. Выведи результат на экран.
- *
- * Date: 31.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level07.lesson09.task05;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * JavaRush.ru
+ * Level 07, Lesson 09, task 05
+ * <p/>
+ * Введи с клавиатуры 10 слов в массив строк. Удвой их по принципу a,b,c -> a,a,b,b,c,c. Выведи результат на экран.
+ * <p/>
+ * Date: 31.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     public static final int LINES_QTY = 10; //Кол-во вводимых строк
 
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
         ArrayList<String> list = new ArrayList<String>();
 
-        for (int i = 0; i < LINES_QTY; i++)
+        for ( int i = 0; i < LINES_QTY; i++ )
         {
             String s = br.readLine();
-            list.add(s);
-            list.add(s);
+            list.add( s );
+            list.add( s );
         }
 
-        for ( String s : list)
+        for ( String s : list )
         {
-            System.out.println(s);
+            System.out.println( s );
         }
     }
 }
