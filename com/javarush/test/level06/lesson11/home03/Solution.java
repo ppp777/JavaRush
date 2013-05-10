9b6e63cc10d4cebe8c5e84ff6278487d7c4ca9d9
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-10 10:01:44 +0300

    com.javarush.test.level06.lesson11.home03 changed.

--- com/javarush/test/level06/lesson11/home03/Solution.java
+++ com/javarush/test/level06/lesson11/home03/Solution.java
@@ -1,24 +1,21 @@
-/**
- *
- *JavaRush.ru
- *Level 06, Lesson 11, home 03
- *
- * Переставить один модификатор static, чтобы пример скомпилировался.
- *
- * Date: 27.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level06.lesson11.home03;
 
+/**
+ * JavaRush.ru
+ * Level 06, Lesson 11, home 03
+ * <p/>
+ * Переставить один модификатор static, чтобы пример скомпилировался.
+ * <p/>
+ * Date: 27.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     public static int A = 5;
     public int B = 2;
     public int C = A * B;
 
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
         A = 15;
     }
