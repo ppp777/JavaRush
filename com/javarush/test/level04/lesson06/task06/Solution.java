fd488174c001f978a8feb3e97c8f74ee93118d08
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-10 20:31:31 +0300

    com.javarush.test.level04.lesson06.task06 changed.

--- com/javarush/test/level04/lesson06/task06/Solution.java
+++ com/javarush/test/level04/lesson06/task06/Solution.java
@@ -1,25 +1,28 @@
 package com.javarush.test.level04.lesson06.task06;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 06, Task 06
- *
+ * JavaRush.ru
+ * Level 04, Lesson 06, Task 06
+ * <p/>
  * Ввести с клавиатуры имя и возраст. Если возраст больше 20 вывести надпись «И 18-ти достаточно»
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
-public class Solution {
-    public static void main(String[] args) throws Exception
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
         String name = reader.readLine();
-        String sAge = reader.readLine();
-        int nAge = Integer.parseInt(sAge);
+        int age = Integer.parseInt( reader.readLine() );
 
-        if (nAge > 20)
-            System.out.println("И 18-ти достаточно");
+        if ( age > 20 )
+        {
+            System.out.println( "И 18-ти достаточно" );
+        }
     }
 }
