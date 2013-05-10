c4a38db57997b7e9feb2ff861a5e8f161627afe4
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-10 09:36:35 +0300

    com.javarush.test.level07.lesson12.home07 changed.

--- com/javarush/test/level07/lesson12/home07/Solution.java
+++ com/javarush/test/level07/lesson12/home07/Solution.java
@@ -1,19 +1,14 @@
 package com.javarush.test.level07.lesson12.home07;
 
 /**
-*
 *JavaRush.ru
 *Level 07, Lesson 12, home07
 *
-*
 * Переставь один модификатор static чтобы пример скомпилировался
-*
 *
 * Date: 02.02.13
 * @author Sergey Kandalintsev
-*
 */
-
 public class Solution
 {
     public static int A = 5;
