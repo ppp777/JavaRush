c097ca071e026fe5fd4b888d0e09d5862e4713a4
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-10 11:27:08 +0300

    com.javarush.test.level05.lesson12.home05 changed.

--- com/javarush/test/level05/lesson12/home05/Solution.java
+++ com/javarush/test/level05/lesson12/home05/Solution.java
@@ -1,32 +1,31 @@
-/**
- *
- *JavaRush.ru
- *Level 05, Lesson 12, home 05
- *
- * Вводить с клавиатуры числа и считать их сумму, пока пользователь не введет слово "сумма".
- * Вывести на экран полученную сумму
- *
- * Date: 25.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level05.lesson12.home05;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * JavaRush.ru
+ * Level 05, Lesson 12, home 05
+ * <p/>
+ * Вводить с клавиатуры числа и считать их сумму, пока пользователь не введет слово "сумма".
+ * Вывести на экран полученную сумму
+ * <p/>
+ * Date: 25.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        String s;
         int sum = 0;
 
-        for (String s = br.readLine(); !s.equals("сумма"); s = br.readLine()) {
-            sum += Integer.parseInt(s);
+        while ( !"сумма".equals( s = reader.readLine() ) )
+        {
+            sum += Integer.parseInt( s );
         }
 
-        System.out.println(sum);
+        System.out.println( sum );
     }
 }
