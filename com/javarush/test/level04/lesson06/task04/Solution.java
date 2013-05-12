d5446424760a5ea52adb71f003ec2fd80c4251a4
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-12 14:44:29 +0300

    com.javarush.test.level04.lesson06.task04 changed.

--- com/javarush/test/level04/lesson06/task04/Solution.java
+++ com/javarush/test/level04/lesson06/task04/Solution.java
@@ -1,27 +1,33 @@
 package com.javarush.test.level04.lesson06.task04;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 06, Task 04
- *
+ * JavaRush.ru
+ * Level 04, Lesson 06, Task 04
+ * <p/>
  * Ввести с клавиатуры два имени, и если имена одинаковые вывести сообщение «Имена идентичны». Если их длины равны –
  * вывести сообщение – «Длины имён равны»
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
-public class Solution {
-    public static void main(String[] args) throws Exception
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
         String firstName = reader.readLine();
         String secondName = reader.readLine();
 
-        if (firstName.equals(secondName))
-            System.out.println("Имена идентичны");
-        if (firstName.length() == secondName.length())
-            System.out.println("Длины имён равны");
+        if ( firstName.equals( secondName ) )
+        {
+            System.out.println( "Имена идентичны" );
+        }
+        if ( firstName.length() == secondName.length() )
+        {
+            System.out.println( "Длины имён равны" );
+        }
     }
 }
