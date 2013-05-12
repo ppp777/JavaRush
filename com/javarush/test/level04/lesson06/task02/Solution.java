c65222511c65db1f214fb05a95eafe14ac317559
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-12 14:38:07 +0300

    com.javarush.test.level04.lesson06.task02 changed.

--- com/javarush/test/level04/lesson06/task02/Solution.java
+++ com/javarush/test/level04/lesson06/task02/Solution.java
@@ -4,31 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 06, Task 02
+ * JavaRush.ru
+ * Level 04, Lesson 06, Task 02
  * Ввести с клавиатуры четыре числа, и вывести максимальное из них.
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
-public class Solution {
-    public static void main(String[] args) throws Exception
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String sFirstNumber = reader.readLine();
-        String sSecondNumber = reader.readLine();
-        String sThirdNumber = reader.readLine();
-        String sFourthNumber = reader.readLine();
-        int nFirstNumber = Integer.parseInt(sFirstNumber);
-        int nSecondNumber = Integer.parseInt(sSecondNumber);
-        int nThirdNumber = Integer.parseInt(sThirdNumber);
-        int nFourthNumber = Integer.parseInt(sFourthNumber);
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        int firstNumber = Integer.parseInt( reader.readLine() );
+        int secondNumber = Integer.parseInt( reader.readLine() );
+        int thirdNumber = Integer.parseInt( reader.readLine() );
+        int fourthNumber = Integer.parseInt( reader.readLine() );
 
-        System.out.println(max(max(nFirstNumber, nSecondNumber),
-                               max(nThirdNumber, nFourthNumber)));
-
+        System.out.println( max( max( firstNumber, secondNumber ),
+                                 max( thirdNumber, fourthNumber ) ) );
     }
-    public static int max(int a, int b) {
+
+    public static int max( int a, int b )
+    {
         return a > b ? a : b;
     }
 }
