0357a423409e9ecb11796e336310316219fdb9e5
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-12 14:37:41 +0300

    com.javarush.test.level04.lesson06.task03 changed.

--- com/javarush/test/level04/lesson06/task03/Solution.java
+++ com/javarush/test/level04/lesson06/task03/Solution.java
@@ -4,52 +4,50 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 06, Task 03
+ * JavaRush.ru
+ * Level 04, Lesson 06, Task 03
  * Ввести с клавиатуры три числа, и вывести их в порядке убывания.
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
-public class Solution  
+public class Solution
 {
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String sFirstNumber = reader.readLine();
-        String sSecondNumber = reader.readLine();
-        String sThirdNumber = reader.readLine();
-        int nFirstNumber = Integer.parseInt(sFirstNumber);
-        int nSecondNumber = Integer.parseInt(sSecondNumber);
-        int nThirdNumber = Integer.parseInt(sThirdNumber);
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        int firstNumber = Integer.parseInt( reader.readLine() );
+        int secondNumber = Integer.parseInt( reader.readLine() );
+        int thirdNumber = Integer.parseInt( reader.readLine() );
 
-        printNumbers(nFirstNumber, nSecondNumber, nThirdNumber);
+        printNumbers( firstNumber, secondNumber, thirdNumber );
     }
-    
-    public static void printNumbers(int a, int b, int c) {
+
+    public static void printNumbers( int a, int b, int c )
+    {
         int tmp;
 
-        if (a < b) 
+        if ( a < b )
         {
             tmp = a;
             a = b;
             b = tmp;
         }
 
-        if (b < c) 
+        if ( b < c )
         {
             tmp = b;
             b = c;
             c = tmp;
         }
 
-        if (a < b) 
+        if ( a < b )
         {
             tmp = a;
             a = b;
             b = tmp;
         }
 
-        System.out.println("" + a + " " + b + " " + c);
+        System.out.println( "" + a + " " + b + " " + c );
     }
 }
