e8ce5bf06d1eb91dd1cb2d1406c6a20a96d0b0c1
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-12 13:16:00 +0300

    com.javarush.test.level03.lesson12.home02 changed.

--- com/javarush/test/level03/lesson12/home02/Solution.java
+++ com/javarush/test/level03/lesson12/home02/Solution.java
@@ -1,5 +1,14 @@
 package com.javarush.test.level03.lesson12.home02;
 
+/**
+ * JavaRush.ru
+ * Level 03, Lesson 12, Home 02
+ * <p/>
+ * Вывести на экран десять раз надпись "Я не хочу изучать Java, я хочу большую зарплату".
+ * <p/>
+ * Date: 09.05.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     public static void main( String[] args )
