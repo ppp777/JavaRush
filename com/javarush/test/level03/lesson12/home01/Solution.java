cfb5be21b3be0b0c494107bff481364ad3409dfa
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-12 13:14:36 +0300

    com.javarush.test.level03.lesson12.home01 changed.

--- com/javarush/test/level03/lesson12/home01/Solution.java
+++ com/javarush/test/level03/lesson12/home01/Solution.java
@@ -1,5 +1,14 @@
 package com.javarush.test.level03.lesson12.home01;
 
+/**
+ * JavaRush.ru
+ * Level 03, Lesson 12, Home 01
+ * <p/>
+ * Вывести на экран надпись "Жить хорошо, а хорошо жить еще лучше".
+ * <p/>
+ * Date: 09.05.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     public static void main( String[] args )
