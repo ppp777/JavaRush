133d67f02e58052d9beac7894dc8153b9a16dba7
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-13 19:14:00 +0300

    com.javarush.test.level05.lesson12.home04 changed.

--- com/javarush/test/level05/lesson12/home04/Solution.java
+++ com/javarush/test/level05/lesson12/home04/Solution.java
@@ -1,26 +1,24 @@
-/**
- *
- *JavaRush.ru
- *Level 05, Lesson 12, home 04
- *
- * Вывести на экран текущую дату в аналогичном виде "21 12 2012".
- *
- * Date: 25.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level05.lesson12.home04;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+/**
+ * JavaRush.ru
+ * Level 05, Lesson 12, home 04
+ * <p/>
+ * Вывести на экран текущую дату в аналогичном виде "21 12 2012".
+ * <p/>
+ * Date: 25.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
-        GregorianCalendar curTime = new GregorianCalendar();
-        System.out.println(formatter.format(curTime.getTime()));
+        SimpleDateFormat formatter = new SimpleDateFormat( "dd MM yyyy" );
+        Calendar curTime = new GregorianCalendar();
+        System.out.println( formatter.format( curTime.getTime() ) );
     }
 }
