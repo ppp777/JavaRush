9cf30d49d855e794fdf1c72ab67b634f94cdc935
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-14 18:37:57 +0300

    com.javarush.test.level05.lesson09.task01 changed.

--- com/javarush/test/level05/lesson09/task01/Friend.java
+++ com/javarush/test/level05/lesson09/task01/Friend.java
@@ -1,34 +1,36 @@
+package com.javarush.test.level05.lesson09.task01;
+
 /**
- *JavaRush.ru
- *Level 05, Lesson 09, Task 01
- *
+ * JavaRush.ru
+ * Level 05, Lesson 09, Task 01
+ * <p/>
  * Создать класс Friend (друг) с тремя конструкторами:
  * - Имя
  * - Имя, возраст
  * - Имя, возраст, пол
- *
+ * <p/>
  * Date: 24.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level05.lesson09.task01;
-
 public class Friend
 {
     private String name;
     private int age;
     private String gender;
 
-    private Friend(String name) {
+    private Friend( String name )
+    {
         this.name = name;
     }
 
-    private Friend(String name, int age) {
+    private Friend( String name, int age )
+    {
         this.name = name;
         this.age = age;
     }
 
-    private Friend(String name, int age, String gender) {
+    private Friend( String name, int age, String gender )
+    {
         this.name = name;
         this.age = age;
         this.gender = gender;
