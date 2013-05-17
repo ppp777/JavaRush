8ee57722baca0e234c46e5418267d91e9b869d06
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-17 21:32:04 +0300

    com.javarush.test.level06.lesson05.task03 changed.

--- com/javarush/test/level06/lesson05/task03/Solution.java
+++ com/javarush/test/level06/lesson05/task03/Solution.java
@@ -1,33 +1,34 @@
+package com.javarush.test.level06.lesson05.task03;
+
 /**
- *JavaRush.ru
- *Level 06, Lesson 05, Task 03
- *
+ * JavaRush.ru
+ * Level 06, Lesson 05, Task 03
+ * <p/>
  * Создать в цикле 10000 объектов Cat и Dog. (Java-машина должна начать уничтожать неиспользуемые и метод finalize
  * хоть раз да вызовется).
- *
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level06.lesson05.task03;
-
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        for (int i = 0; i < 10000; i++) {
+        for ( int i = 0; i < 10000; i++ )
+        {
             new Cat();
             new Dog();
         }
     }
 }
+
 class Cat
 {
     @Override
     protected void finalize() throws Throwable
     {
         super.finalize();
-        System.out.println("Cat destroyed");
+        System.out.println( "Cat destroyed" );
     }
 }
 
@@ -37,6 +38,6 @@ class Dog
     protected void finalize() throws Throwable
     {
         super.finalize();
-        System.out.println("Dog destroyed");
+        System.out.println( "Dog destroyed" );
     }
 }
