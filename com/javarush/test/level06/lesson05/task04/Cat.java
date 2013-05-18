d1c0027e7742d16a9b4f80171297f69675ac8cd6
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-18 21:34:08 +0300

    com.javarush.test.level06.lesson05.task04 changed.

--- com/javarush/test/level06/lesson05/task04/Cat.java
+++ com/javarush/test/level06/lesson05/task04/Cat.java
@@ -1,25 +1,27 @@
+package com.javarush.test.level06.lesson05.task04;
+
 /**
- *JavaRush.ru
- *Level 06, Lesson 05, Task 04
- *
+ * JavaRush.ru
+ * Level 06, Lesson 05, Task 04
+ * <p/>
  * В конструкторе класса Cat увеличивать счётчик котов (статическую переменную этого же класса) на 1.
  * В методе finalize (уменьшать на 1).
- *
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level06.lesson05.task04;
-
 public class Cat
 {
     static int catCount;
 
-    public Cat() {
+    public Cat()
+    {
         ++catCount;
     }
 
-    protected void finalize() {
+    @Override
+    protected void finalize()
+    {
         --catCount;
     }
 }
