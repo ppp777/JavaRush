fb4c453ce39e15e800135693835f46ee69ab87f3
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-19 17:08:12 +0300

    com.javarush.test.level06.lesson08.task01 changed.

--- com/javarush/test/level06/lesson08/task01/Cat.java
+++ com/javarush/test/level06/lesson08/task01/Cat.java
@@ -1,21 +1,21 @@
+package com.javarush.test.level06.lesson08.task01;
+
 /**
- *JavaRush.ru
- *Level 06, Lesson 08, Task 01
- *
+ * JavaRush.ru
+ * Level 06, Lesson 08, Task 01
+ * <p/>
  * Создать класс Cat, в нем статическую переменную catCount. В конструкторе класса Cat, нужно увеличивать
  * данную переменную на 1, при создании нового объекта.
- *
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level06.lesson08.task01;
-
 public class Cat
 {
     static int catCount;
 
-    public Cat() {
+    public Cat()
+    {
         catCount++;
     }
 }
