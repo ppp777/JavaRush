f043d856c6e5ac91654cae7e6bc25ae56b7a92bf
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-20 19:08:45 +0300

    com.javarush.test.level06.lesson08.task02 changed.

--- com/javarush/test/level06/lesson08/task02/Cat.java
+++ com/javarush/test/level06/lesson08/task02/Cat.java
@@ -1,16 +1,15 @@
+package com.javarush.test.level06.lesson08.task02;
+
 /**
- *JavaRush.ru
- *Level 06, Lesson 08, Task 02
- *
+ * JavaRush.ru
+ * Level 06, Lesson 08, Task 02
+ * <p/>
  * Добавить  к классу Cat, два статических метода: int getCatCount(), и setCatCount(int), с помощью
  * которых можно получить/изменить количество котов (переменную catCount).
- *
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level06.lesson08.task02;
-
 public class Cat
 {
     private static int catCount = 0;
@@ -25,7 +24,7 @@ public class Cat
         return catCount;
     }
 
-    public static void setCatCount(int catCount)
+    public static void setCatCount( int catCount )
     {
         Cat.catCount = catCount;
     }
