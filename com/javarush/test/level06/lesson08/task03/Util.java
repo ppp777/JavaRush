0e8b7a1e2abd612750da42908fb3003bce938544
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-21 21:11:14 +0300

    com.javarush.test.level06.lesson08.task03 changed.

--- com/javarush/test/level06/lesson08/task03/Util.java
+++ com/javarush/test/level06/lesson08/task03/Util.java
@@ -1,19 +1,19 @@
 package com.javarush.test.level06.lesson08.task03;
 
 /**
- *JavaRush.ru
- *Level 06, Lesson 08, Task 03
- *
+ * JavaRush.ru
+ * Level 06, Lesson 08, Task 03
+ * <p/>
  * Сделать класс Util, с единственным статическим методом double getDistance(x1, y1, x2, y2) – метод
  * должен вычислять расстояние между точками.
- *
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
 public class Util
 {
-    public static double getDistance(int x1, int y1, int x2, int y2)
+    public static double getDistance( int x1, int y1, int x2, int y2 )
     {
-        return Math.sqrt( (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );
+        return Math.sqrt( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );
     }
 }
