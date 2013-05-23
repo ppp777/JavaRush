dffe54936360cbc765fbba00938e876d93cf28b3
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-23 21:58:54 +0300

    com.javarush.test.level06.lesson08.task05 changed.

--- com/javarush/test/level06/lesson08/task05/StringHelper.java
+++ com/javarush/test/level06/lesson08/task05/StringHelper.java
@@ -1,32 +1,32 @@
+package com.javarush.test.level06.lesson08.task05;
+
 /**
- *JavaRush.ru
- *Level 06, Lesson 08, Task 05
- *
- * Cделать класс StringHelper, у которого будут 2 статических метода:
+ * JavaRush.ru
+ * Level 06, Lesson 08, Task 05
+ * <p/>
+ * Сделать класс StringHelper, у которого будут 2 статических метода:
  * String multiply(String s, int count) – возвращает строку повторенную count раз.
  * String multiply(String s) – возвращает строку повторенную 5 раз.
  * Пример:
- *  Амиго -> АмигоАмигоАмигоАмигоАмиго
- *
+ * Амиго -> АмигоАмигоАмигоАмигоАмиго
+ * <p/>
  * Date: 26.01.13
  * @author Sergey Kandalintsev
  */
-
-package com.javarush.test.level06.lesson08.task05;
-
 public class StringHelper
 {
-    public static String multiply(String s)
+    public static String multiply( String s )
     {
-        return multiply(s, 5);
+        return multiply( s, 5 );
     }
 
-    public static String multiply(String s, int count)
+    public static String multiply( String s, int count )
     {
-        StringBuffer buffer = new StringBuffer(25);
+        StringBuffer buffer = new StringBuffer( 25 );
 
-        for (int i = 0; i < count; i++) {
-            buffer.append(s);
+        for ( int i = 0; i < count; i++ )
+        {
+            buffer.append( s );
         }
 
         return buffer.toString();
