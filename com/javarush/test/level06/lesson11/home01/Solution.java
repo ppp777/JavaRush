e8132738bc53bf8eb1f14a6743a6bdb9eec71826
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-25 01:22:18 +0300

    com.javarush.test.level06.lesson11.home01 changed.

--- com/javarush/test/level06/lesson11/home01/Solution.java
+++ com/javarush/test/level06/lesson11/home01/Solution.java
@@ -1,27 +1,25 @@
-/**
- *
- *JavaRush.ru
- *Level 06, Lesson 11, home 01
- *
- * Создай класс кот - Cat. В нем создай статическую переменную catCount. Пусть при каждом создании кота эта переменная
- * увеличивается на 1. Создать 10 объектов Cat и вывести  значение переменной catCount на экран.      их всех на экран, пользуясь переменной cats
- *
- * Date: 27.01.13
- * @author Sergey Kandalintsev
- *
- */
 package com.javarush.test.level06.lesson11.home01;
 
+/**
+ * JavaRush.ru
+ * Level 06, Lesson 11, home 01
+ * <p/>
+ * Создай класс кот - Cat. В нем создай статическую переменную catCount. Пусть при каждом создании кота эта переменная
+ * увеличивается на 1. Создать 10 объектов Cat и вывести  значение переменной catCount на экран.
+ * <p/>
+ * Date: 27.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        for (int i = 0; i < 10; i++)
+        for ( int i = 0; i < 10; i++ )
         {
             new Cat();
         }
 
-        System.out.println(Cat.catCount);
+        System.out.println( Cat.catCount );
     }
 
     public static class Cat
