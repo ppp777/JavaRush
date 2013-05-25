1b7b06383940e1ed026380c5589aea584dd11963
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-25 21:43:09 +0300

    com.javarush.test.level06.lesson11.home05 changed.

--- com/javarush/test/level06/lesson11/home05/Solution.java
+++ com/javarush/test/level06/lesson11/home05/Solution.java
@@ -1,25 +1,23 @@
-/**
- *
- *JavaRush.ru
- *Level 06, Lesson 11, home 05
- *
- * Вывести на экран таблицу умножения 10х10
- *
- * Date: 27.01.13
- * @author Sergey Kandalintsev
- *
- */
 package com.javarush.test.level06.lesson11.home05;
 
+/**
+ * JavaRush.ru
+ * Level 06, Lesson 11, home 05
+ * <p/>
+ * Вывести на экран таблицу умножения 10х10
+ * <p/>
+ * Date: 27.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        for (int i = 1; i <= 10; i++)
+        for ( int i = 1; i <= 10; i++ )
         {
-            for (int j = 1; j <= 10; j++)
+            for ( int j = 1; j <= 10; j++ )
             {
-                System.out.print(i * j + " ");
+                System.out.print( i * j + " " );
             }
             System.out.println();
         }
