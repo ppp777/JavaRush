cd03d865f2b55f9f34b024066d3dfc8ea7c481dd
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-26 18:53:10 +0300

    com.javarush.test.level06.lesson11.home06 changed.

--- com/javarush/test/level06/lesson11/home06/Solution.java
+++ com/javarush/test/level06/lesson11/home06/Solution.java
@@ -1,28 +1,24 @@
-/**
- *
- *JavaRush.ru
- *Level 06, Lesson 11, home 05
- *
- * Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
- *
- * Date: 27.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level06.lesson11.home06;
 
+/**
+ * JavaRush.ru
+ * Level 06, Lesson 11, home 05
+ * <p/>
+ * Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+ * <p/>
+ * Date: 27.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
         KissMyShinyMetalAss kiss = new KissMyShinyMetalAss();
-        System.out.println(kiss);
+        System.out.println( kiss );
     }
 
     public static class KissMyShinyMetalAss
     {
 
     }
-
 }
