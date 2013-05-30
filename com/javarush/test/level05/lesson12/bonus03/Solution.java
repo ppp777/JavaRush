2caa309a0961198150f8f5282ca229f045b60fe0
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-30 21:53:15 +0300

    com.javarush.test.level05.lesson12.bonus03 changed.

--- com/javarush/test/level05/lesson12/bonus03/Solution.java
+++ com/javarush/test/level05/lesson12/bonus03/Solution.java
@@ -1,38 +1,36 @@
-/**
- *
- *JavaRush.ru
- *Level 05, Lesson 12, bonus 03
- *
- * Задача: Написать программу, которая вводит 10 чисел с клавиатуры и выводит максимальное из них на экран
- *
- * Date: 25.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level05.lesson12.bonus03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * JavaRush.ru
+ * Level 05, Lesson 12, bonus 03
+ * <p/>
+ * Задача: Написать программу, которая вводит 10 чисел с клавиатуры и выводит максимальное из них на экран
+ * <p/>
+ * Date: 25.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
-    public static void main(String[] args) throws Exception
+    public static final int NUMBERS_QTY = 10;   //Кол-во вводимых чисел.
+
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int nextNumber, maximum;
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+        int maximum = Integer.parseInt( reader.readLine() );
 
-        nextNumber = Integer.parseInt(reader.readLine());
-        maximum = nextNumber;
-
-        for (int i = 0; i < 9; i++) {
-            if (nextNumber > maximum) {
+        for ( int i = 1; i < NUMBERS_QTY; i++ )
+        {
+            int nextNumber = Integer.parseInt( reader.readLine() );
+            if ( nextNumber > maximum )
+            {
                 maximum = nextNumber;
             }
-            nextNumber = Integer.parseInt(reader.readLine());
         }
 
-        System.out.println(maximum);
+        System.out.println( maximum );
     }
 }
 
