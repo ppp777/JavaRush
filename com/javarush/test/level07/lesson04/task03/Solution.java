6f0d909a4af81413bd736bd4a6c1a0fd54428a72
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-05-31 19:34:10 +0300

    com.javarush.test.level07.lesson04.task03 changed.

--- com/javarush/test/level07/lesson04/task03/Solution.java
+++ com/javarush/test/level07/lesson04/task03/Solution.java
@@ -1,42 +1,39 @@
-/**
- *
- *JavaRush.ru
- *Level 07, Lesson 04, task 03
- *
- * Создать массив на 10 строк. Создать массив на 10 чисел. Ввести с клавиатуры 10 строк. В массив чисел,
- * в каждую ячейку записать длину строки из массива строк, с таким же индексом/номером ячейки.
- * Вывести содержимое массива чисел на экран.
- *
- * Date: 28.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level07.lesson04.task03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * JavaRush.ru
+ * Level 07, Lesson 04, task 03
+ * <p/>
+ * Создать массив на 10 строк. Создать массив на 10 чисел. Ввести с клавиатуры 10 строк. В массив чисел,
+ * в каждую ячейку записать длину строки из массива строк, с таким же индексом/номером ячейки.
+ * Вывести содержимое массива чисел на экран.
+ * <p/>
+ * Date: 28.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     final static int LINES_QTY = 10; //Кол-во строк в массивах
 
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
         String[] s = new String[LINES_QTY];
         int[] sLength = new int[LINES_QTY];
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 
-        for (int i = 0; i < s.length; i++)
+        for ( int i = 0; i < s.length; i++ )
         {
             s[i] = br.readLine();
             sLength[i] = s[i].length();
         }
 
         System.out.println();
-        for (int i = 0; i < s.length; i++)
+        for ( int i = 0; i < s.length; i++ )
         {
-            System.out.println(sLength[i]);
+            System.out.println( sLength[i] );
         }
     }
 }
