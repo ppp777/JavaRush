c5cec9c53ebc55369441d7f10a819c0e2be5ba0e
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-01 21:35:38 +0300

    com.javarush.test.level04.lesson02.task03 added.

---
+++ com/javarush/test/level04/lesson02/task03/Cat.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level04.lesson02.task03;
+
+/**
+ * Date: 01.06.13
+ * @author Sergey Kandalintsev
+ */
+public class Cat
+{
+    private static int catsCount = 0;
+
+    public static void setCatsCount( int catsCount )
+    {
+        Cat.catsCount = catsCount;
+    }
+
+}
