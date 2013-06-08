8a5deb053de557b3d05f0e2a75df78aaebe08b73
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-08 16:03:14 +0300

    com.javarush.test.level07.lesson09.task01 changed.

--- com/javarush/test/level07/lesson09/task01/Solution.java
+++ com/javarush/test/level07/lesson09/task01/Solution.java
@@ -1,73 +1,72 @@
-/**
- *
- *JavaRush.ru
- *Level 07, Lesson 09, task 01
- *
- * Введи с клавиатуры 20 чисел, сохрани их в список и рассортируй по трём массивам:
- *  Число делится на 3 (x%3==0), делится на 2 (x%2==0) и все остальные.
- *  Выведи эти три массива на экран (числа, которые делятся на 3 и на 2, например 6, попадают только в первый массив).
- *
- * Date: 30.01.13
- * @author Sergey Kandalintsev
- *
- */
-
 package com.javarush.test.level07.lesson09.task01;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * JavaRush.ru
+ * Level 07, Lesson 09, task 01
+ * <p/>
+ * Введи с клавиатуры 20 чисел, сохрани их в список и рассортируй по трём массивам:
+ * Число делится на 3 (x%3==0), делится на 2 (x%2==0) и все остальные.
+ * Выведи эти три массива на экран (числа, которые делятся на 3 и на 2, например 6, попадают только в первый массив).
+ * <p/>
+ * Date: 30.01.13
+ * @author Sergey Kandalintsev
+ */
 public class Solution
 {
     public static final int N = 20;
 
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
         ArrayList<Integer> list = new ArrayList<Integer>();
         ArrayList<Integer> even = new ArrayList<Integer>();
         ArrayList<Integer> multipleOf3 = new ArrayList<Integer>();
         ArrayList<Integer> others = new ArrayList<Integer>();
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 
-        for (int i = 0; i < N; i++)
+        for ( int i = 0; i < N; i++ )
         {
-            list.add( Integer.parseInt(br.readLine()) );
+            list.add( Integer.parseInt( br.readLine() ) );
         }
 
-        for (Integer x : list)
+        for ( int x : list )
         {
-            if (x % 3 == 0)
+            if ( x % 3 == 0 )
             {
-                multipleOf3.add(x);
+                multipleOf3.add( x );
             }
-            else if (x % 2 == 0)
+            else if ( x % 2 == 0 )
             {
-                even.add(x);
+                even.add( x );
             }
             else
             {
-                others.add(x);
+                others.add( x );
             }
         }
 
-        for (Integer x : multipleOf3)
+        System.out.println();
+
+        for ( int x : multipleOf3 )
         {
-            System.out.println(x);
+            System.out.println( x );
         }
 
         System.out.println();
 
-        for (Integer x : even)
+        for ( int x : even )
         {
-            System.out.println(x);
+            System.out.println( x );
         }
 
         System.out.println();
 
-        for (Integer x : others)
+        for ( int x : others )
         {
-            System.out.println(x);
+            System.out.println( x );
         }
     }
 }
