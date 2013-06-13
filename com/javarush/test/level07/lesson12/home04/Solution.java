e1f3dd7a615648e65c04898278ab472cf4141f37
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-13 18:56:17 +0300

    com.javarush.test.level07.lesson12.home04 changed.

--- com/javarush/test/level07/lesson12/home04/Solution.java
+++ com/javarush/test/level07/lesson12/home04/Solution.java
@@ -6,34 +6,31 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
- *
- *JavaRush.ru
- *Level 07, Lesson 12, home04
- *
+ * JavaRush.ru
+ * Level 07, Lesson 12, home04
+ * <p/>
  * Создать список строк. Ввести строки с клавиатуры и добавить их в список. Вводить с клавиатуры строки, пока
  * пользователь не введет пустую строку (нажмет Enter). Вывести строки на экран.
- *
- *
+ * <p/>
  * Date: 01.02.13
  * @author Sergey Kandalintsev
- *
  */
 
 public class Solution
 {
-    public static void main(String[] args) throws IOException
+    public static void main( String[] args ) throws IOException
     {
         ArrayList<String> list = new ArrayList<String>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
 
-        for (String s = reader.readLine(); !s.equals(""); s = reader.readLine())
+        for ( String s = reader.readLine(); !s.isEmpty(); s = reader.readLine() )
         {
-            list.add(s);
+            list.add( s );
         }
 
-        for (String s : list)
+        for ( String s : list )
         {
-            System.out.println(s);
+            System.out.println( s );
         }
     }
 }
