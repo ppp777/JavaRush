5221a8cc2194332e10bfd5e9003264edf38ba911
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-27 17:49:49 +0300

    com.javarush.test.level04.lesson02.task03 changed.

--- com/javarush/test/level04/lesson02/task03/Cat.java
+++ com/javarush/test/level04/lesson02/task03/Cat.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level04.lesson02.task03;
 
 /**
+ * JavaRush.ru
+ * Level 04, Lesson 02, Task 03
+ * <br>
+ * Вывести на экран десять раз надпись "Я не хочу изучать Java, я хочу большую зарплату".
+ * <br>
  * Date: 01.06.13
  * @author Sergey Kandalintsev
  */
@@ -12,5 +17,4 @@ public class Cat
     {
         Cat.catsCount = catsCount;
     }
-
 }
