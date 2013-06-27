6f192991155494c794720ffb5dccc25f245b203d
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-27 18:02:17 +0300

    com.javarush.test.level04.lesson10.task02 changed.

--- com/javarush/test/level04/lesson10/task02/Solution.java
+++ com/javarush/test/level04/lesson10/task02/Solution.java
@@ -1,20 +1,23 @@
 package com.javarush.test.level04.lesson10.task02;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 10, Task 02
- *
+ * JavaRush.ru
+ * Level 04, Lesson 10, Task 02
+ * br>
  * Вывести на экран числа от 10 до 1
- *
+ * <br>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args) {
+    public static void main( String[] args )
+    {
         int i = 10;
-        while (i >= 1)
-            System.out.println(i--);
 
+        while ( i >= 1 )
+        {
+            System.out.println( i-- );
+        }
     }
 }
