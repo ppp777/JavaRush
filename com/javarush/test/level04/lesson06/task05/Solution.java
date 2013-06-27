82d97567fb886906de8510b750834c2d9feb838a
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-27 17:56:44 +0300

    com.javarush.test.level04.lesson06.task05 changed.

--- com/javarush/test/level04/lesson06/task05/Solution.java
+++ com/javarush/test/level04/lesson06/task05/Solution.java
@@ -4,23 +4,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 06, Task 05
- *
+ * JavaRush.ru
+ * Level 04, Lesson 06, Task 05
+ * <br>
  * Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти ещё»
- *
+ * <br>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
-public class Solution {
-    public static void main(String[] args) throws Exception
+public class Solution
+{
+    public static void main( String[] args ) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
         String name = reader.readLine();
         String sAge = reader.readLine();
-        int nAge = Integer.parseInt(sAge);
+        int nAge = Integer.parseInt( sAge );
 
-        if (nAge < 18)
-            System.out.println("Подрасти ещё");
+        if ( nAge < 18 )
+        {
+            System.out.println( "Подрасти ещё" );
+        }
     }
 }
