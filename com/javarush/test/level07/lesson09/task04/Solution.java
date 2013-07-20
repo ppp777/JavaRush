3778cb2f24a6adfa69a9640f41cbf47bc704d67c
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-07-20 17:01:53 +0300

    com.javarush.test.level07.lesson09.task04 changed.

--- com/javarush/test/level07/lesson09/task04/Solution.java
+++ com/javarush/test/level07/lesson09/task04/Solution.java
@@ -1,14 +1,12 @@
 /**
- *
  *JavaRush.ru
  *Level 07, Lesson 09, task 04
- *
+ * <p/>
  * Создай список из слов «мама», «мыла», «раму». Удали все слова содержащие букву «р» и удвой все слова
  * содержащие букву «л».
- *
+ * <p/>
  * Date: 31.01.13
  * @author Sergey Kandalintsev
- *
  */
 
 package com.javarush.test.level07.lesson09.task04;
@@ -18,33 +16,33 @@ import java.util.Collections;
 
 public class Solution
 {
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
         ArrayList<String> list = new ArrayList<String>();
 
-        Collections.addAll(list, "мама", "мыла", "раму");
-        for (int i = 0; i < list.size(); ) {
-            if ( list.get(i).contains("р") )
+        Collections.addAll( list, "мама", "мыла", "раму" );
+        for ( int i = 0; i < list.size(); )
+        {
+            if ( list.get( i ).contains( "р" ) )
             {
-                list.remove(i);
+                list.remove( i );
                 continue;
             }
             i++;
         }
 
-        for (int i = 0; i < list.size(); )
+        for ( int i = 0; i < list.size(); i++ )
         {
-            if ( list.get(i).contains("л") )
+            if ( list.get( i ).contains( "л" ) )
             {
-                list.add( i, list.get(i) );
+                list.add( i, list.get( i ) );
                 i++;
             }
-            i++;
         }
 
-        for (String s : list)
+        for ( String s : list )
         {
-            System.out.println(s);
+            System.out.println( s );
         }
     }
 }
