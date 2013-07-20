d38a29ada155740bbe7c4266b07caa1181b5d623
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-07-20 14:08:40 +0300

    com.javarush.test.level09.lesson08.task02.

--- com/javarush/test/level09/lesson08/task02/Solution.java
+++ com/javarush/test/level09/lesson08/task02/Solution.java
@@ -4,12 +4,12 @@ package com.javarush.test.level09.lesson08.task02;
  * JavaRush.ru
  * Level 09, Lesson 08, Task 02
  * <p/>
- * Есть три исключения, последовательно унаследованные от Exception: <b />
- * class Exception1 extends Exception <b />
- * class Exception2 extends Exception1 <b />
- * Exception3 extends Exception2 <b />
- * Есть метод, который описан так: <b />
- * public static void method1() throws Exception1, Exception2, Exception3 <b />
+ * Есть три исключения, последовательно унаследованные от Exception: <br />
+ * class Exception1 extends Exception <br />
+ * class Exception2 extends Exception1 <br />
+ * Exception3 extends Exception2 <br />
+ * Есть метод, который описан так: <br />
+ * public static void method1() throws Exception1, Exception2, Exception3 <br />
  * Напишите catch, который перехватит все три Exception1, Exception2, Exception3.
  * <p/>
  * Date: 09.04.13
@@ -17,18 +17,6 @@ package com.javarush.test.level09.lesson08.task02;
  */
 public class Solution
 {
-    static class Exception1 extends Exception
-    {
-    }
-
-    static class Exception2 extends Exception1
-    {
-    }
-
-    static class Exception3 extends Exception2
-    {
-    }
-
     public static void main( String[] args ) throws Exception
     {
         try
@@ -57,4 +45,16 @@ public class Solution
             throw new Exception3();
         }
     }
+}
+
+class Exception1 extends Exception
+{
+}
+
+class Exception2 extends Exception1
+{
+}
+
+class Exception3 extends Exception2
+{
 }
