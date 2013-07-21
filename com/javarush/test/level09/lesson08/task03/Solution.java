6022c496657d5e6b73a02b8ef9d95e291e2e6ef8
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-07-21 14:11:44 +0300

    com.javarush.test.level09.lesson08.task03 changed.

--- com/javarush/test/level09/lesson08/task03/Solution.java
+++ com/javarush/test/level09/lesson08/task03/Solution.java
@@ -4,28 +4,14 @@ package com.javarush.test.level09.lesson08.task03;
  * JavaRush.ru
  * Level 09, Lesson 08, Task 03
  * <p/>
- * То же задание что и level09.lesson08.task02, но: <b />
- * Напиши catch, который перехватит Exception1, Exception3, но не Exception2. <b />
- * Подсказка:<b />
- * <p/>
+ * То же задание что и level09.lesson08.task02, но: <br />
+ * Напиши catch, который перехватит Exception1, Exception3, но не Exception2. <br />
  * <p/>
  * Date: 09.04.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    static class Exception1 extends Exception
-    {
-    }
-
-    static class Exception2 extends Exception1
-    {
-    }
-
-    static class Exception3 extends Exception2
-    {
-    }
-
     public static void main( String[] args ) throws Exception
     {
         try
@@ -63,3 +49,16 @@ public class Solution
         }
     }
 }
+
+class Exception1 extends Exception
+{
+}
+
+class Exception2 extends Exception1
+{
+}
+
+class Exception3 extends Exception2
+{
+}
+
