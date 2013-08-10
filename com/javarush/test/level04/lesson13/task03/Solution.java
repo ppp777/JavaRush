14c8b3e7dea67ec92f701a1a61613db5d62d6d65
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-08-10 17:12:54 +0300

    com.javarush.test.level04.lesson13.task03 done.

--- com/javarush/test/level04/lesson13/task03/Solution.java
+++ com/javarush/test/level04/lesson13/task03/Solution.java
@@ -1,20 +1,24 @@
 package com.javarush.test.level04.lesson13.task03;
 
 /**
- *JavaRush.ru
- *Level 04, Lesson 13, Task 03
- *
+ * JavaRush.ru <br/>
+ * Level 04, Lesson 13, Task 03
+ * <p/>
  * Вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
- *
+ * <p/>
  * Date: 20.01.13
  * @author Sergey Kandalintsev
  */
 public class Solution
 {
-    public static void main(String[] args) {
-        for (int i = 10; i > 0; i--) {
-            for (int j = 0; j < i; j++)
-                System.out.print("8");
+    public static void main( String[] args )
+    {
+        for ( int i = 10; i > 0; i-- )
+        {
+            for ( int j = 0; j < i; j++ )
+            {
+                System.out.print( "8" );
+            }
             System.out.println();
         }
     }
